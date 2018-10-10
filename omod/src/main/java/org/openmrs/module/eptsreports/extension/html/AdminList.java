2a1146eb5659f4a854f020c80eae3e025bada04e
Nathan Floor <nathan.floor@jembi.org>
2018-10-10 10:33:06 +0200

    Updated gitignore to exclude project files for IDEs and compiled build target files

--- omod/src/main/java/org/openmrs/module/eptsreports/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/eptsreports/extension/html/AdminList.java
@@ -17,7 +17,8 @@ import org.openmrs.module.web.extension.AdministrationSectionExt;
 
 /**
  * This class defines the links that will appear on the administration page under the
- * "eptsreports.title" heading. This extension is enabled by defining (uncommenting) it in the config.xml file.
+ * "eptsreports.title" heading. This extension is enabled by defining (uncommenting) it in the
+ * config.xml file.
  */
 public class AdminList extends AdministrationSectionExt {
 	
