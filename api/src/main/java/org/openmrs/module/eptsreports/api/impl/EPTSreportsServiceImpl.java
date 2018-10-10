12726429d3e30e025fbb72f320cc6b543d2d9f79
Nathan Floor <nathan.floor@jembi.org>
2018-10-10 10:23:05 +0200

    Scaffolding - creating openmrs app

---
+++ api/src/main/java/org/openmrs/module/eptsreports/api/impl/EPTSreportsServiceImpl.java
@@ -0,0 +1,52 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.eptsreports.api.impl;
+
+import org.openmrs.api.APIException;
+import org.openmrs.api.UserService;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.eptsreports.Item;
+import org.openmrs.module.eptsreports.api.EPTSreportsService;
+import org.openmrs.module.eptsreports.api.dao.EPTSreportsDao;
+
+public class EPTSreportsServiceImpl extends BaseOpenmrsService implements EPTSreportsService {
+	
+	EPTSreportsDao dao;
+	
+	UserService userService;
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setDao(EPTSreportsDao dao) {
+		this.dao = dao;
+	}
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Override
+	public Item getItemByUuid(String uuid) throws APIException {
+		return dao.getItemByUuid(uuid);
+	}
+	
+	@Override
+	public Item saveItem(Item item) throws APIException {
+		if (item.getOwner() == null) {
+			item.setOwner(userService.getUser(1));
+		}
+		
+		return dao.saveItem(item);
+	}
+}
