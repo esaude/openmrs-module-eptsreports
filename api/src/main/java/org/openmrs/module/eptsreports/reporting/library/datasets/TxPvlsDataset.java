7ea49e440df47f5c13418fe24202bbd4888789a0
ningosi <nicotwendelee@gmail.com>
2018-11-09 15:52:38 +0300

    Tie the dimensions to the data set definitions

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TxPvlsDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TxPvlsDataset.java
@@ -1,8 +1,12 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
+import org.openmrs.module.eptsreports.reporting.library.dimensions.EptsCommonDimension;
+import org.openmrs.module.eptsreports.reporting.library.indicators.HivIndicators;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -10,12 +14,22 @@ import java.util.List;
 
 @Component
 public class TxPvlsDataset {
+
+	@Autowired
+	private HivIndicators hivIndicators;
+
+	@Autowired
+	private EptsCommonDimension eptsCommonDimension;
 	
 	public CohortIndicatorDataSetDefinition constructTxPvlsDatset() {
 		
 		CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
 		dsd.setName("Tx_Pvls Dataset");
 		dsd.addParameters(getParameters());
+		//tie dimensions to this data definition
+		dsd.addDimension("gender", EptsReportUtils.map(eptsCommonDimension.gender(), ""));
+		dsd.addDimension("age", EptsReportUtils.map(eptsCommonDimension.age(), "effectiveDate=${endDate}"));
+		dsd.addDimension("pb", EptsReportUtils.map(eptsCommonDimension.breastfeedingAndPregnant(), "startDate=${startDate},endDate=${endDate},location={location}"));
 		
 		return dsd;
 		
