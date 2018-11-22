54b0d2c8b65ff59af0ee69076869e335ec71cfee
ningosi <nicotwendelee@gmail.com>
2018-11-22 10:33:44 +0300

    Provide meaningful names to the parameters in the pregnancy class

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/PregnantQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/PregnantQueries.java
@@ -9,12 +9,13 @@ public class PregnantQueries {
 	 * Looks for patients indicated PREGNANT in the initial or follow-up consultation between start date
 	 * and end date
 	 */
-	public static String getPregnantOnInitialOrFollowUpConsulation(int c1, int c2, int e1, int e2) {
+	public static String getPregnantOnInitialOrFollowUpConsulation(int pregnant, int gestation, int adultInEnc,
+	        int adultSegEnc) {
 		
 		return "SELECT p.patient_id" + " FROM patient p" + " INNER JOIN encounter e ON p.patient_id=e.patient_id"
 		        + " INNER JOIN obs o ON e.encounter_id=o.encounter_id"
-		        + " WHERE p.voided=0 and e.voided=0 and o.voided=0 and concept_id=" + c1 + " AND value_coded=" + c2
-		        + " AND e.encounter_type IN (" + e1 + "," + e2 + ")"
+		        + " WHERE p.voided=0 and e.voided=0 and o.voided=0 and concept_id=" + pregnant + " AND value_coded="
+		        + gestation + " AND e.encounter_type IN (" + adultInEnc + "," + adultSegEnc + ")"
 		        + " AND e.encounter_datetime BETWEEN :startDate AND :endDate AND e.location_id=:location";
 		
 	}
@@ -23,31 +24,31 @@ public class PregnantQueries {
 	 * Looks for patients with Number of Weeks Pregnant registered in the initial or follow-up
 	 * consultation
 	 */
-	public static String getWeeksPregnantOnInitialOrFollowUpConsultations(int c1, int e1, int e2) {
+	public static String getWeeksPregnantOnInitialOrFollowUpConsultations(int numOfWeeks, int adultInEnc, int adultSegEnc) {
 		return "SELECT p.patient_id" + " FROM patient p INNER JOIN encounter e ON p.patient_id=e.patient_id"
 		        + " INNER JOIN obs o ON e.encounter_id=o.encounter_id"
-		        + " WHERE p.voided=0 and e.voided=0 and o.voided=0 and concept_id=" + c1 + " AND e.encounter_type IN (" + e1
-		        + "," + e2 + ")" + " AND e.encounter_datetime BETWEEN :startDate AND :endDate AND"
-		        + " e.location_id=:location";
+		        + " WHERE p.voided=0 and e.voided=0 and o.voided=0 and concept_id=" + numOfWeeks
+		        + " AND e.encounter_type IN (" + adultInEnc + "," + adultSegEnc + ")"
+		        + " AND e.encounter_datetime BETWEEN :startDate AND :endDate AND" + " e.location_id=:location";
 	}
 	
 	/**
 	 * Looks for patients with PREGNANCY DUE DATE registered in the initial or follow-up consultation
 	 * between start date and end date
 	 */
-	public static String getPregnancyDueDateRegistred(int c1, int e1, int e2) {
+	public static String getPregnancyDueDateRegistred(int dueDate, int adultInEnc, int adultSegEnc) {
 		return "SELECT p.patient_id FROM patient p INNER JOIN encounter"
 		        + " e ON p.patient_id=e.patient_id INNER JOIN obs o ON e.encounter_id=o.encounter_id"
-		        + " WHERE p.voided=0 AND e.voided=0 AND o.voided=0 AND concept_id=" + c1 + " AND e.encounter_type IN (" + e1
-		        + "," + e2 + ")" + " AND e.encounter_datetime BETWEEN :startDate AND :endDate AND"
+		        + " WHERE p.voided=0 AND e.voided=0 AND o.voided=0 AND concept_id=" + dueDate + " AND e.encounter_type IN ("
+		        + adultInEnc + "," + adultSegEnc + ")" + " AND e.encounter_datetime BETWEEN :startDate AND :endDate AND"
 		        + " e.location_id=:location";
 	}
 	
 	/**
 	 * Looks for patients enrolled on PTV/ETV program between start date and end date
 	 */
-	public static String getEnrolledInPtvOrEtv(int p) {
-		return "SELECT pp.patient_id FROM patient_program pp WHERE pp.program_id=" + p
+	public static String getEnrolledInPtvOrEtv(int ptvProgram) {
+		return "SELECT pp.patient_id FROM patient_program pp WHERE pp.program_id=" + ptvProgram
 		        + " AND pp.voided=0 AND pp.date_enrolled BETWEEN " + ":startDate AND :endDate AND pp.location_id=:location";
 	}
 	
