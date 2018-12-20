debec39fa1ca9c8b66785d6775cb1881b296ab89
Nathan Floor <nathan.floor@jembi.org>
2018-12-20 08:07:06 +0200

    REmoved unused import

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/pvls/InitialArtStartDateCalculation.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/pvls/InitialArtStartDateCalculation.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.openmrs.Concept;
 import org.openmrs.Encounter;
