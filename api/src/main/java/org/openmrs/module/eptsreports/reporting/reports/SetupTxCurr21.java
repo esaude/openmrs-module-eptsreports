4c3827a6068f758820b6851f33144664d6f0a63a
Nathan Floor <nathan.floor@jembi.org>
2018-12-20 08:38:49 +0200

    Renamed txCurr60 -> txCurr2.1

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTxCurr21.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTxCurr21.java
@@ -24,7 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SetupTxCurr60 extends EptsDataExportManager {
+public class SetupTxCurr21 extends EptsDataExportManager {
 	
 	@Autowired
 	private TxCurrDataset txCurrDataset;
@@ -32,7 +32,7 @@ public class SetupTxCurr60 extends EptsDataExportManager {
 	@Autowired
 	private GenericCohortQueries genericCohortQueries;
 	
-	public SetupTxCurr60() {
+	public SetupTxCurr21() {
 	}
 	
 	@Override
@@ -52,7 +52,7 @@ public class SetupTxCurr60 extends EptsDataExportManager {
 	
 	@Override
 	public String getName() {
-		return "TX_CURR Report 60";
+		return "TX_CURR Report 2.1";
 	}
 	
 	@Override
@@ -81,7 +81,7 @@ public class SetupTxCurr60 extends EptsDataExportManager {
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		ReportDesign reportDesign = null;
 		try {
-			reportDesign = createXlsReportDesign(reportDefinition, "TXCURR.xls", "TXCURR_60.xls_", getExcelDesignUuid(), null);
+			reportDesign = createXlsReportDesign(reportDefinition, "TXCURR.xls", "TXCURR_2.1.xls_", getExcelDesignUuid(), null);
 			Properties props = new Properties();
 			props.put("repeatingSections", "sheet:1,dataset:TX_CURR Data Set");
 			props.put("sortWeight", "5000");
