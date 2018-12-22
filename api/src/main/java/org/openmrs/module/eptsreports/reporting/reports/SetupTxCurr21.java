5d5f759ec489263fdbf0f4e1428e8619a49d991c
Edrisse <edrisseasm@gmail.com>
2018-12-22 17:55:17 +0200

    Re enabling TxCurr 2.1

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTxCurr21.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTxCurr21.java
@@ -23,7 +23,6 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Deprecated
 @Component
 public class SetupTxCurr21 extends EptsDataExportManager {
 	
