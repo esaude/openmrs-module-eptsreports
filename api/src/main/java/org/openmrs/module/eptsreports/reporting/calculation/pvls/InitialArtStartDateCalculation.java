0e3f9c3cddd75d0ad3946e1227a6a3f4ba0ce15f
guimino <minoneves@gmail.com>
2018-12-27 17:03:58 +0200

    removing null dates in enrollmentDates to prevent NPE on sorting list

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/pvls/InitialArtStartDateCalculation.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/pvls/InitialArtStartDateCalculation.java
@@ -1,15 +1,13 @@
 /*
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+ * The contents of this file are subject to the OpenMRS Public License Version
+ * 1.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://license.openmrs.org
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
  *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ * Copyright (C) OpenMRS, LLC. All Rights Reserved.
  */
 package org.openmrs.module.eptsreports.reporting.calculation.pvls;
 
@@ -103,6 +101,7 @@ public class InitialArtStartDateCalculation extends AbstractPatientCalculation {
 				enrollmentDates.add(transferInObs.getObsDatetime());
 			}
 			if (enrollmentDates.size() > 0) {
+				enrollmentDates.removeAll(Collections.singleton(null));
 				Collections.sort(enrollmentDates);
 				requiredDate = enrollmentDates.get(0);
 			}
