3d72451007cf22ec8aefe83944a6eac0f3c9643b
ningosi <nicotwendelee@gmail.com>
2019-01-23 17:54:47 +0300

    Optimize imports

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/pvls/RoutineCalculation.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/pvls/RoutineCalculation.java
@@ -21,7 +21,6 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.openmrs.Concept;
 import org.openmrs.EncounterType;
