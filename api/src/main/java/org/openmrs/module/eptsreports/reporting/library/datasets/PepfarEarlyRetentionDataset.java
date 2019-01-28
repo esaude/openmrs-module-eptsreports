5742d1650c6699648e39585c64ce7d32d69e1d58
ningosi <nicotwendelee@gmail.com>
2019-01-28 17:07:28 +0300

    Adding the cohort class that will house all the cohort definiton for this data set

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/PepfarEarlyRetentionDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/PepfarEarlyRetentionDataset.java
@@ -1,16 +1,24 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
+import org.openmrs.module.eptsreports.reporting.library.cohorts.PepfarEarlyRetentionCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PepfarEarlyRetentionDataset extends BaseDataSet{
+	@Autowired
+	private PepfarEarlyRetentionCohortQueries pepfarEarlyRetentionCohortQueries;
+
+	@Autowired
+	private EptsGeneralIndicator eptsGeneralIndicator;
 
     public DataSetDefinition constructPepfarEarlyRetentionDatset() {
         CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
         String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
-        dsd.setName("Tx_Pvls Data Set");
+        dsd.setName("Pepfar early retention Data Set");
         dsd.addParameters(getParameters());
         return dsd;
     }
