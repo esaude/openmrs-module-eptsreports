be3ff88bed0685bfb8dfa0437656597458fa6d88
guimino <minoneves@gmail.com>
2019-02-11 16:30:03 +0200

    MEPTS-79: replaced @Deprecated annotation

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTxPvls.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTxPvls.java
@@ -26,6 +26,7 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Deprecated
 @Component
 public class SetupTxPvls extends EptsDataExportManager {
 
