80763cc23ab751ad9352712b1c82e9e76df0805c
Nathan Floor <nathan.floor@jembi.org>
2019-02-18 14:01:25 +0200

    MEPTS-83: missing formatter changes

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/calculation/BasePatientCalculationTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/calculation/BasePatientCalculationTest.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,101 +22,102 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 // TODO probably move this into calculation module
 public abstract class BasePatientCalculationTest extends BaseModuleContextSensitiveTest {
-	
-	@Autowired
-	protected TestsHelper testsHelper;
-	
-	@Autowired
-	protected OpenMRSTestHelper openmrsTestHelper;
-	
-	protected PatientCalculationService service;
-	
-	private PatientCalculationContext evaluationContext;
-	
-	protected Map<String, Object> params = new HashMap<String, Object>();
-	
-	public abstract PatientCalculation getCalculation();
-	
-	public abstract Collection<Integer> getCohort();
-	
-	public abstract CalculationResultMap getResult();
-	
-	@Before
-	public void setUp() throws Exception {
-		service = Context.getService(PatientCalculationService.class);
-		evaluationContext = service.createCalculationContext();
-		
-		Map<String, Object> cacheEntries = new HashMap<String, Object>();
-		cacheEntries.put("location", Context.getLocationService().getLocation(1));
-		setEvaluationContext(testsHelper.getDate("2019-05-30 00:00:00.0"));
-		setEvaluationContext(cacheEntries);
-		
-		executeDataSet("calculationsTest.xml");
-		
-		PatientCalculation calculation = getCalculation();
-		new EptsCalculationsMock(calculation);
-		new EptsCalculationUtilsMock();
-	}
-	
-	/** This test runs for all classes that extend this class, it's the basic calculation unit test */
-	@Test
-	public void evaluateShouldReturnMatchedResultMapBySizeAndPrintOutGivenCalculationCohort() {
-		CalculationResultMap result = getResult();
-		PatientCalculation calculation = getCalculation();
-		Collection<Integer> cohort = getCohort();
-		Assert.assertNotNull(calculation);
-		Assert.assertNotNull(cohort);
-		Assert.assertNotNull(result);
-		CalculationResultMap evaluatedResult = service.evaluate(cohort, calculation, params, getEvaluationContext());
-		Assert.assertTrue(matchCalculationResults(result, evaluatedResult));
-	}
-	
-	protected void setEvaluationContext(Date now) {
-		evaluationContext.setNow(now == null ? new Date() : now);
-	}
-	
-	@SuppressWarnings("rawtypes")
-	protected void setEvaluationContext(Map<String, Object> cacheEntries) {
-		if (cacheEntries != null && !cacheEntries.isEmpty()) {
-			for (Map.Entry e : cacheEntries.entrySet()) {
-				evaluationContext.addToCache((String) e.getKey(), e.getValue());
-			}
-		}
-	}
-	
-	public PatientCalculationContext getEvaluationContext() {
-		return evaluationContext;
-	}
-	
-	protected void matchOtherResultsExcept(CalculationResultMap evaluatedResult, Integer... patientsIdNotToMatch) {
-		CalculationResultMap otherResult = (CalculationResultMap) evaluatedResult.clone();
-		CalculationResultMap initialResult = (CalculationResultMap) getResult().clone();
-		if (patientsIdNotToMatch != null) {
-			for (int i : patientsIdNotToMatch) {
-				initialResult.remove(i);
-				otherResult.remove(i);
-			}
-		}
-		Assert.assertTrue(matchCalculationResults(initialResult, otherResult));
-	}
-	
-	protected boolean matchCalculationResults(CalculationResultMap results1, CalculationResultMap results2) {
-		boolean matched = true;
-		if (results1.size() != results2.size() || !results1.keySet().equals(results2.keySet())) {
-			matched = false;
-		} else {
-			Iterator<Integer> results1Iterator = results1.keySet().iterator();
-			while (results1Iterator.hasNext()) {
-				Integer key = results1Iterator.next();
-				if (results1.get(key).getValue() == null) {
-					matched = results2.get(key).getValue() == null;
-				} else if (results2.get(key).getValue() == null) {
-					matched = results1.get(key).getValue() == null;
-				} else if (!results1.get(key).getValue().equals(results2.get(key).getValue())) {
-					matched = false;
-				}
-			}
-		}
-		return matched;
-	}
+
+  @Autowired protected TestsHelper testsHelper;
+
+  @Autowired protected OpenMRSTestHelper openmrsTestHelper;
+
+  protected PatientCalculationService service;
+
+  private PatientCalculationContext evaluationContext;
+
+  protected Map<String, Object> params = new HashMap<String, Object>();
+
+  public abstract PatientCalculation getCalculation();
+
+  public abstract Collection<Integer> getCohort();
+
+  public abstract CalculationResultMap getResult();
+
+  @Before
+  public void setUp() throws Exception {
+    service = Context.getService(PatientCalculationService.class);
+    evaluationContext = service.createCalculationContext();
+
+    Map<String, Object> cacheEntries = new HashMap<String, Object>();
+    cacheEntries.put("location", Context.getLocationService().getLocation(1));
+    setEvaluationContext(testsHelper.getDate("2019-05-30 00:00:00.0"));
+    setEvaluationContext(cacheEntries);
+
+    executeDataSet("calculationsTest.xml");
+
+    PatientCalculation calculation = getCalculation();
+    new EptsCalculationsMock(calculation);
+    new EptsCalculationUtilsMock();
+  }
+
+  /** This test runs for all classes that extend this class, it's the basic calculation unit test */
+  @Test
+  public void evaluateShouldReturnMatchedResultMapBySizeAndPrintOutGivenCalculationCohort() {
+    CalculationResultMap result = getResult();
+    PatientCalculation calculation = getCalculation();
+    Collection<Integer> cohort = getCohort();
+    Assert.assertNotNull(calculation);
+    Assert.assertNotNull(cohort);
+    Assert.assertNotNull(result);
+    CalculationResultMap evaluatedResult =
+        service.evaluate(cohort, calculation, params, getEvaluationContext());
+    Assert.assertTrue(matchCalculationResults(result, evaluatedResult));
+  }
+
+  protected void setEvaluationContext(Date now) {
+    evaluationContext.setNow(now == null ? new Date() : now);
+  }
+
+  @SuppressWarnings("rawtypes")
+  protected void setEvaluationContext(Map<String, Object> cacheEntries) {
+    if (cacheEntries != null && !cacheEntries.isEmpty()) {
+      for (Map.Entry e : cacheEntries.entrySet()) {
+        evaluationContext.addToCache((String) e.getKey(), e.getValue());
+      }
+    }
+  }
+
+  public PatientCalculationContext getEvaluationContext() {
+    return evaluationContext;
+  }
+
+  protected void matchOtherResultsExcept(
+      CalculationResultMap evaluatedResult, Integer... patientsIdNotToMatch) {
+    CalculationResultMap otherResult = (CalculationResultMap) evaluatedResult.clone();
+    CalculationResultMap initialResult = (CalculationResultMap) getResult().clone();
+    if (patientsIdNotToMatch != null) {
+      for (int i : patientsIdNotToMatch) {
+        initialResult.remove(i);
+        otherResult.remove(i);
+      }
+    }
+    Assert.assertTrue(matchCalculationResults(initialResult, otherResult));
+  }
+
+  protected boolean matchCalculationResults(
+      CalculationResultMap results1, CalculationResultMap results2) {
+    boolean matched = true;
+    if (results1.size() != results2.size() || !results1.keySet().equals(results2.keySet())) {
+      matched = false;
+    } else {
+      Iterator<Integer> results1Iterator = results1.keySet().iterator();
+      while (results1Iterator.hasNext()) {
+        Integer key = results1Iterator.next();
+        if (results1.get(key).getValue() == null) {
+          matched = results2.get(key).getValue() == null;
+        } else if (results2.get(key).getValue() == null) {
+          matched = results1.get(key).getValue() == null;
+        } else if (!results1.get(key).getValue().equals(results2.get(key).getValue())) {
+          matched = false;
+        }
+      }
+    }
+    return matched;
+  }
 }
--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/calculation/CountingCalculationTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/calculation/CountingCalculationTest.java
@@ -9,24 +9,24 @@ import org.openmrs.calculation.result.SimpleResult;
 
 // TODO probably move this into calculation module
 public class CountingCalculationTest extends BasePatientCalculationTest {
-	
-	@Override
-	public PatientCalculation getCalculation() {
-		return new CountingCalculation();
-	}
-	
-	@Override
-	public Collection<Integer> getCohort() {
-		return Arrays.asList(new Integer[] { 1, 2, 3 });
-	}
-	
-	@Override
-	public CalculationResultMap getResult() {
-		PatientCalculation calculation = getCalculation();
-		CalculationResultMap map = new CalculationResultMap();
-		map.put(1, new SimpleResult(1, calculation, getEvaluationContext()));
-		map.put(2, new SimpleResult(2, calculation, getEvaluationContext()));
-		map.put(3, new SimpleResult(3, calculation, getEvaluationContext()));
-		return map;
-	}
+
+  @Override
+  public PatientCalculation getCalculation() {
+    return new CountingCalculation();
+  }
+
+  @Override
+  public Collection<Integer> getCohort() {
+    return Arrays.asList(new Integer[] {1, 2, 3});
+  }
+
+  @Override
+  public CalculationResultMap getResult() {
+    PatientCalculation calculation = getCalculation();
+    CalculationResultMap map = new CalculationResultMap();
+    map.put(1, new SimpleResult(1, calculation, getEvaluationContext()));
+    map.put(2, new SimpleResult(2, calculation, getEvaluationContext()));
+    map.put(3, new SimpleResult(3, calculation, getEvaluationContext()));
+    return map;
+  }
 }
