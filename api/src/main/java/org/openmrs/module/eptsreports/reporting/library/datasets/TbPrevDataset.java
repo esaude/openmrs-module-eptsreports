ef72041330d57b7bd9aa4e685eef53826fcd186c
Edrisse <edrisseasm@gmail.com>
2019-03-07 09:04:56 +0200

    Numerator colum working

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TbPrevDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TbPrevDataset.java
@@ -13,15 +13,37 @@
  */
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
+import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TbPrevDataset extends BaseDataSet {
 
+  @Autowired private GenericCohortQueries genericCohortQueries;
+
+  @Autowired private EptsGeneralIndicator eptsGeneralIndicator;
+
   public DataSetDefinition constructTbPrevDatset() {
     CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+    String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
+    dsd.setName("TB PREV Data Set");
+    dsd.addParameters(getParameters());
+    dsd.addColumn(
+        "NUM-TOTAL",
+        "Numerator Total",
+        EptsReportUtils.map(
+            eptsGeneralIndicator.getIndicator(
+                "Numerator Total",
+                EptsReportUtils.map(
+                    genericCohortQueries.getStartedArtOnPeriod(false),
+                    "onOrAfter=${startDate},onOrBefore=${endDate},location=${location}")),
+            mappings),
+        "");
     return dsd;
   }
 }
