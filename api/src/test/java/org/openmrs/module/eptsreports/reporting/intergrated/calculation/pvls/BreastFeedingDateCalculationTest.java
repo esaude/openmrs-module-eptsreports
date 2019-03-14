b0a834562e894c14cceb32c5733c5ea0768b8d05
ningosi <nicotwendelee@gmail.com>
2019-03-14 12:06:39 +0300

    Re write the BreastFeedingDateCalculationTest intergration test

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/calculation/pvls/BreastFeedingDateCalculationTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/calculation/pvls/BreastFeedingDateCalculationTest.java
@@ -1,10 +1,6 @@
 package org.openmrs.module.eptsreports.reporting.intergrated.calculation.pvls;
 
-import java.util.Arrays;
-import java.util.Collection;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculation;
 import org.openmrs.calculation.patient.PatientCalculationContext;
@@ -12,6 +8,10 @@ import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.calculation.result.SimpleResult;
 import org.openmrs.module.eptsreports.reporting.calculation.pvls.BreastfeedingDateCalculation;
 import org.openmrs.module.eptsreports.reporting.intergrated.calculation.BasePatientCalculationTest;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class BreastFeedingDateCalculationTest extends BasePatientCalculationTest {
 
@@ -33,15 +33,34 @@ public class BreastFeedingDateCalculationTest extends BasePatientCalculationTest
 
     PatientCalculationContext evaluationContext = getEvaluationContext();
 
-    map.put(91, new SimpleResult(true, calculation, evaluationContext));
-
-    map.put(501, new SimpleResult(true, calculation, evaluationContext));
-
-    map.put(7, new SimpleResult(true, calculation, evaluationContext));
-
-    map.put(8, new SimpleResult(false, calculation, evaluationContext));
-
-    map.put(90, new SimpleResult(true, calculation, evaluationContext));
+    //Date marked as breastFeeding : 2018-01-21
+      map.put(
+              501,
+              new SimpleResult(
+                      new Timestamp(testsHelper.getDate("2018-01-21 00:00:00.0").getTime()),
+                      calculation,
+                      evaluationContext));
+      //BreastfeedingCalculation.hasHIVStartDate(2018-06-20 00:00:00.0, List<Obs>)
+      map.put(
+              7,
+              new SimpleResult(
+                      new Timestamp(testsHelper.getDate("2018-06-20 00:00:00.0").getTime()),
+                      calculation,
+                      evaluationContext));
+      //BreastfeedingCalculation.hasDeliveryDate(2018-07-01 00:00:00.0, List<Obs>)
+      map.put(
+              7,
+              new SimpleResult(
+                      new Timestamp(testsHelper.getDate("2018-07-01 00:00:00.0").getTime()),
+                      calculation,
+                      evaluationContext));
+      //BreastfeedingCalculation.isBreastFeedingInProgram(2018-08-30 00:00:00.0, List<PatientState>)
+      map.put(
+              7,
+              new SimpleResult(
+                      new Timestamp(testsHelper.getDate("2018-08-30 00:00:00.0").getTime()),
+                      calculation,
+                      evaluationContext));
     return map;
   }
 
@@ -50,80 +69,4 @@ public class BreastFeedingDateCalculationTest extends BasePatientCalculationTest
     executeDataSet("pvlsTest.xml");
   }
 
-  /*
-   *
-   * Last VL Date: 2019-01-21
-   *
-   * Date marked as breastFeeding : 2018-01-21
-   *
-   *
-   * rule tested: BreastfeedingCalculation.isLactating(Date, List<Obs>)
-   */
-  @Test
-  public void shouldEvaluatePatientMarkedAsBreastFeedingByLactatinng() {
-
-    setEvaluationContext(testsHelper.getDate("2019-01-30 00:00:00.0"));
-
-    CalculationResultMap evaluatedResult =
-        service.evaluate(getCohort(), getCalculation(), getEvaluationContext());
-    Assert.assertEquals(
-        testsHelper.getDate("2018-01-21 00:00:00.0"), evaluatedResult.get(501).getValue());
-    matchOtherResultsExcept(evaluatedResult, 7, 8, 90, 91);
-  }
-
-  /*
-   * rule tested : BreastfeedingCalculation.hasHIVStartDate(Date, List<Obs>)
-   */
-  @Test
-  public void shouldEvaluatePatientMarkedAsBreastFeedingByHIVStartDate() {
-
-    CalculationResultMap evaluatedResult =
-        service.evaluate(getCohort(), getCalculation(), getEvaluationContext());
-    Assert.assertEquals(
-        testsHelper.getDate("2018-06-20 00:00:00.0"), evaluatedResult.get(7).getValue());
-    matchOtherResultsExcept(evaluatedResult, 501, 8, 90, 91);
-  }
-
-  /*
-   * Patient ID : 91
-   *
-   * Report end Period Date:
-   *
-   * Deliver date:
-   *
-   * Last Viral load date:
-   *
-   * rule tested: BreastfeedingCalculation.hasDeliveryDate(Date, List<Obs>)
-   */
-  @Test
-  public void shouldEvaluatePatientMarkedAsBreastFeedingByDeliveryDate() {
-
-    CalculationResultMap evaluatedResult =
-        service.evaluate(getCohort(), getCalculation(), getEvaluationContext());
-    Assert.assertEquals(
-        testsHelper.getDate("2018-07-01 00:00:00.0"), evaluatedResult.get(91).getValue());
-    matchOtherResultsExcept(evaluatedResult, 501, 7, 90, 8);
-  }
-
-  /*
-   * Patient ID : 8
-   *
-   * Report end Period Date: 2025-12-30 == 2019-04-30
-   *
-   * Patient State Start Date: 2018-08-30
-   *
-   * Last VL Date: 2025-12-30
-   *
-   * rule tested: BreastfeedingCalculation.isBreastFeedingInProgram(Date,
-   * List<PatientState>)
-   */
-  @Test
-  public void shouldEvaluatePatientMarkedAsBreastFeedingByPatientState() {
-
-    CalculationResultMap evaluatedResult =
-        service.evaluate(getCohort(), getCalculation(), getEvaluationContext());
-    Assert.assertEquals(
-        testsHelper.getDate("2018-08-30 00:00:00.0"), evaluatedResult.get(90).getValue());
-    matchOtherResultsExcept(evaluatedResult, 501, 7, 91, 8);
-  }
 }
