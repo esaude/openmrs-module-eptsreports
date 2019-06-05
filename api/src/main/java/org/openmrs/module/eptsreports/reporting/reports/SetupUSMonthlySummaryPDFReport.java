a5acf91574989f0ae874ad1f557c00592a3ce658
Nathan Floor <nathan.floor@jembi.org>
2019-06-05 13:02:18 +0200

    Depricated USMonthly Summary report

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupUSMonthlySummaryPDFReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupUSMonthlySummaryPDFReport.java
@@ -13,6 +13,7 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Deprecated
 @Component
 public class SetupUSMonthlySummaryPDFReport extends EptsDataExportManager {
 
