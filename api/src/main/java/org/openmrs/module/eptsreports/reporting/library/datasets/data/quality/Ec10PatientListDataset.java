33561409711a6db39cd2f3e47329f7e80a85d5e7
ningosi <nicotwendelee@gmail.com>
2019-07-18 19:42:49 +0300

    Added EC10 to use sql dataset definition and mapped to the template

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/data/quality/Ec10PatientListDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/data/quality/Ec10PatientListDataset.java
@@ -13,18 +13,12 @@
  */
 package org.openmrs.module.eptsreports.reporting.library.datasets.data.quality;
 
-import java.util.Arrays;
 import java.util.List;
 import org.openmrs.module.eptsreports.metadata.HivMetadata;
-import org.openmrs.module.eptsreports.reporting.library.cohorts.data.quality.SummaryDataQualityCohorts;
-import org.openmrs.module.eptsreports.reporting.library.converter.EncounterDataConverter;
-import org.openmrs.module.eptsreports.reporting.library.converter.PatientProgramDataConverter;
-import org.openmrs.module.eptsreports.reporting.library.data.definition.DataDefinitions;
 import org.openmrs.module.eptsreports.reporting.library.datasets.BaseDataSet;
-import org.openmrs.module.eptsreports.reporting.utils.EptsCommonUtils;
-import org.openmrs.module.reporting.common.TimeQualifier;
+import org.openmrs.module.eptsreports.reporting.library.queries.DqQueries;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
-import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,74 +26,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class Ec10PatientListDataset extends BaseDataSet {
 
-  private SummaryDataQualityCohorts summaryDataQualityCohorts;
-
   private HivMetadata hivMetadata;
 
-  private EptsCommonUtils eptsCommonUtils;
-
-  private DataDefinitions definitions;
-
   @Autowired
-  public Ec10PatientListDataset(
-      SummaryDataQualityCohorts summaryDataQualityCohorts,
-      HivMetadata hivMetadata,
-      EptsCommonUtils eptsCommonUtils,
-      DataDefinitions definitions) {
-    this.summaryDataQualityCohorts = summaryDataQualityCohorts;
+  public Ec10PatientListDataset(HivMetadata hivMetadata) {
     this.hivMetadata = hivMetadata;
-    this.eptsCommonUtils = eptsCommonUtils;
-    this.definitions = definitions;
   }
 
   public DataSetDefinition ec10PatientListDataset(List<Parameter> parameterList) {
-    PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+    SqlDataSetDefinition dsd = new SqlDataSetDefinition();
     dsd.setName("EC10");
     dsd.addParameters(parameterList);
-    dsd.addRowFilter(
-        summaryDataQualityCohorts.getPatientsWithStatesAndEncounters(
+    dsd.setSqlQuery(
+        DqQueries.getEc10CombinedQuery(
             hivMetadata.getARTProgram().getProgramId(),
             hivMetadata.getAbandonedWorkflowState().getProgramWorkflowStateId(),
-            Arrays.asList(
-                hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
-                hivMetadata.getARVPediatriaSeguimentoEncounterType().getEncounterTypeId())),
-        "location=${location}");
-
-    // add standard column
-    eptsCommonUtils.addStandardColumns(dsd);
-    dsd.addColumn(
-        "Patient Enrollment Date in TARV",
-        definitions.getPatientProgramEnrollment(hivMetadata.getARTProgram(), TimeQualifier.FIRST),
-        "enrolledOnOrBefore=${endDate}",
-        new PatientProgramDataConverter("date"));
-    dsd.addColumn(
-        "Last Patient Status in Prog Enrollment",
-        definitions.getPatientProgramEnrollment(hivMetadata.getARTProgram(), TimeQualifier.FIRST),
-        "enrolledOnOrBefore=${endDate}",
-        new PatientProgramDataConverter("lastStatus"));
-    dsd.addColumn(
-        "Date of Last Patient Status in Prog Enrollment",
-        definitions.getPatientProgramEnrollment(hivMetadata.getARTProgram(), TimeQualifier.FIRST),
-        "enrolledOnOrBefore=${endDate}",
-        new PatientProgramDataConverter("lastStatusDate"));
-    dsd.addColumn(
-        "Clinical Consultation Date",
-        definitions.getEncounterForPatient(
-            Arrays.asList(
-                hivMetadata.getARVPediatriaSeguimentoEncounterType(),
-                hivMetadata.getAdultoSeguimentoEncounterType())),
-        "",
-        new EncounterDataConverter("encounterDate"));
-
-    dsd.addColumn(
-        "Clinical Consultation Registration Date",
-        definitions.getEncounterForPatient(
-            Arrays.asList(
-                hivMetadata.getARVPediatriaSeguimentoEncounterType(),
-                hivMetadata.getAdultoSeguimentoEncounterType())),
-        "",
-        new EncounterDataConverter("encounterCreatedDate"));
-
+            hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
+            hivMetadata.getARVPediatriaSeguimentoEncounterType().getEncounterTypeId()));
     return dsd;
   }
 }
--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/DqQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/DqQueries.java
@@ -386,4 +386,46 @@ public class DqQueries {
             + ")";
     return query;
   }
+
+  /**
+   * Get the query to be used to display the EC10 patient listing
+   *
+   * @return String
+   */
+  public static String getEc10CombinedQuery(
+      int programId, int stateId, int adultFollowUp, int childFollowUp) {
+    String query =
+        "SELECT DISTINCT(pa.patient_id), pi.identifier AS NID, CONCAT(pn.given_name, ' ', pn.family_name ) AS Name, DATE_FORMAT(pe.birthdate, '%d-%m-%Y') AS birthdate, IF(pe.birthdate_estimated = 1, 'Yes','No') AS Estimated_dob, pe.gender AS Sex, DATE_FORMAT(pa.date_created, '%d-%m-%Y %H:%i:%s') AS First_entry_date, DATE_FORMAT(pa.date_changed, '%d-%m-%Y %H:%i:%s') AS Last_updated, DATE_FORMAT(pg.date_enrolled, '%d-%m-%Y %H:%i:%s') AS date_enrolled, ps.state AS state, DATE_FORMAT(ps.start_date, '%d-%m-%Y %H:%i:%s') AS state_date FROM patient pa "
+            + " INNER JOIN patient_identifier pi ON pa.patient_id=pi.patient_id"
+            + " INNER JOIN person pe ON pa.patient_id=pe.person_id"
+            + " INNER JOIN person_name pn ON pa.patient_id=pn.person_id "
+            + " INNER JOIN patient_program pg ON pa.patient_id=pg.patient_id "
+            + " INNER JOIN patient_state ps ON pg.patient_program_id=ps.patient_program_id "
+            + " WHERE "
+            + " pg.program_id="
+            + programId
+            + " AND ps.start_date IS NOT NULL AND ps.end_date IS NULL "
+            + " AND pa.patient_id IN("
+            + " SELECT states.patient_id FROM "
+            + " ((SELECT pg.patient_id AS patient_id, ps.start_date AS start_date "
+            + " FROM patient p "
+            + " INNER JOIN patient_program pg ON p.patient_id=pg.patient_id "
+            + " INNER JOIN patient_state ps ON pg.patient_program_id=ps.patient_program_id "
+            + " WHERE pg.voided=0 AND ps.voided=0 AND p.voided=0 AND pg.program_id="
+            + programId
+            + " AND ps.state="
+            + stateId
+            + " AND pg.location_id IN(:location) AND ps.end_date is null GROUP BY pg.patient_id) states INNER JOIN "
+            + " (SELECT p.patient_id AS patient_id, MAX(e.encounter_datetime) AS encounter_date FROM "
+            + " patient p INNER JOIN encounter e ON p.patient_id=e.patient_id WHERE p.voided = 0 and e.voided=0 "
+            + " AND e.encounter_type IN("
+            + adultFollowUp
+            + ","
+            + childFollowUp
+            + ")"
+            + " AND e.location_id IN(:location) GROUP BY p.patient_id "
+            + ") encounter ON states.patient_id=encounter.patient_id) WHERE encounter.encounter_date > states.start_date "
+            + ")";
+    return query;
+  }
 }
