1641a63b2a243536e80232687779ebf6890f7829
ningosi <nicotwendelee@gmail.com>
2019-07-19 12:32:51 +0300

    Added EC6 dataset as an sql dataset definition and provided its corresponding mappings

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/DqQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/DqQueries.java
@@ -601,4 +601,47 @@ public class DqQueries {
             + ")";
     return query;
   }
+
+  /**
+   * Get the combine query for EC6 patient listing report
+   *
+   * @return String
+   */
+  public static String getEc6CombinedQuery(
+      int identifierType, int programId, int stateId, int drugPickupEncounterType) {
+    String query =
+        "SELECT DISTINCT(pa.patient_id), pi.identifier AS NID, CONCAT(pn.given_name, ' ', pn.family_name ) AS Name, DATE_FORMAT(pe.birthdate, '%d-%m-%Y') AS birthdate, IF(pe.birthdate_estimated = 1, 'Yes','No') AS Estimated_dob, pe.gender AS Sex, DATE_FORMAT(pa.date_created, '%d-%m-%Y %H:%i:%s') AS First_entry_date, DATE_FORMAT(pa.date_changed, '%d-%m-%Y %H:%i:%s') AS Last_updated, DATE_FORMAT(pg.date_enrolled, '%d-%m-%Y') AS date_enrolled, ps.state AS state, DATE_FORMAT(ps.start_date, '%d-%m-%Y') AS state_date, DATE_FORMAT(e.encounter_datetime, '%d-%m-%Y') AS encounter_date, DATE_FORMAT(e.date_created, '%d-%m-%Y %H:%i:%s') AS encounter_date_created FROM patient pa "
+            + " INNER JOIN patient_identifier pi ON pa.patient_id=pi.patient_id"
+            + " INNER JOIN person pe ON pa.patient_id=pe.person_id"
+            + " INNER JOIN person_name pn ON pa.patient_id=pn.person_id "
+            + " INNER JOIN patient_program pg ON pa.patient_id=pg.patient_id "
+            + " INNER JOIN patient_state ps ON pg.patient_program_id=ps.patient_program_id "
+            + " INNER JOIN encounter e ON pa.patient_id=e.patient_id "
+            + " WHERE "
+            + " pg.program_id="
+            + programId
+            + " AND pi.identifier_type="
+            + identifierType
+            + " AND e.encounter_type="
+            + drugPickupEncounterType
+            + " AND e.encounter_datetime > ps.start_date"
+            + " AND ps.start_date IS NOT NULL AND ps.end_date IS NULL "
+            + " AND pa.patient_id IN(SELECT states.patient_id FROM "
+            + " ((SELECT pg.patient_id AS patient_id, ps.start_date AS start_date "
+            + " FROM patient p "
+            + " INNER JOIN patient_program pg ON p.patient_id=pg.patient_id "
+            + " INNER JOIN patient_state ps ON pg.patient_program_id=ps.patient_program_id "
+            + " WHERE pg.voided=0 AND ps.voided=0 AND p.voided=0 AND pg.program_id="
+            + programId
+            + " AND ps.state="
+            + stateId
+            + " AND pg.location_id IN(:location) AND ps.end_date IS NULL GROUP BY pg.patient_id) states INNER JOIN "
+            + " (SELECT p.patient_id AS patient_id, MAX(e.encounter_datetime) AS encounter_date FROM "
+            + " patient p INNER JOIN encounter e ON p.patient_id=e.patient_id WHERE p.voided = 0 and e.voided=0 "
+            + " AND e.encounter_type="
+            + drugPickupEncounterType
+            + " AND e.location_id IN(:location) GROUP BY p.patient_id "
+            + ") encounter ON states.patient_id=encounter.patient_id) WHERE encounter.encounter_date > states.start_date)";
+    return query;
+  }
 }
