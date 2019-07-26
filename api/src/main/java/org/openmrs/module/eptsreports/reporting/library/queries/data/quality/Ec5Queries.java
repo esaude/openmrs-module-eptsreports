226364dd6bbdeebde431a1333ccf6e76ae8a7f09
ningosi <nicotwendelee@gmail.com>
2019-07-26 12:28:20 +0300

    Update the template with the latest v3

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/data/quality/Ec5Queries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/data/quality/Ec5Queries.java
@@ -23,7 +23,7 @@ public class Ec5Queries {
   public static String getEc5CombinedQuery(
       int identifierType, int programId, int stateId, int labEncounterType) {
     String query =
-        "SELECT patient_id, NID\tName, birthdate, Estimated_dob, Sex, First_entry_date, Last_updated, date_enrolled, state, state_date, death_date, MIN(lab_form_date) AS lab_form_date, lab_form_date_created FROM("
+        "SELECT patient_id, NID,Name, birthdate, Estimated_dob, Sex, First_entry_date, Last_updated, date_enrolled, state, state_date, death_date, MIN(lab_form_date) AS lab_form_date, lab_form_date_created FROM("
             + " SELECT pa.patient_id, pi.identifier AS NID, CONCAT(pn.given_name, ' ', pn.family_name ) AS Name, DATE_FORMAT(pe.birthdate, '%d-%m-%Y') AS birthdate, IF(pe.birthdate_estimated = 1, 'Yes','No') AS Estimated_dob, pe.gender AS Sex, DATE_FORMAT(pa.date_created, '%d-%m-%Y %H:%i:%s') AS First_entry_date, DATE_FORMAT(pa.date_changed, '%d-%m-%Y %H:%i:%s') AS Last_updated, DATE_FORMAT(pg.date_enrolled, '%d-%m-%Y') AS date_enrolled, case when ps.state = 9 then 'DROPPED FROM TREATMENT' when ps.state = 6 then 'ACTIVE ON PROGRAM' when ps.state = 10 then 'PATIENT HAS DIED' when ps.state = 8 then 'SUSPENDED TREATMENT' when ps.state = 7 then 'TRANSFERED OUT TO ANOTHER FACILITY' when ps.state = 29 then 'TRANSFERRED FROM OTHER FACILTY' end AS state, DATE_FORMAT(ps.start_date, '%d-%m-%Y') AS state_date, DATE_FORMAT(pe.death_date,'%d-%m-%Y') As death_date, DATE_FORMAT(e.encounter_datetime, '%d-%m-%Y') AS lab_form_date, DATE_FORMAT(e.date_created, '%d-%m-%Y %H:%i:%s') AS lab_form_date_created "
             + " FROM patient pa "
             + " INNER JOIN patient_identifier pi ON pa.patient_id=pi.patient_id "
