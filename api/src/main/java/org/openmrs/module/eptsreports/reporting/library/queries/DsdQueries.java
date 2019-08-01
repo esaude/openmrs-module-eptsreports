7174ff632d30512065cd689eaf7a058804d2930a
Nathan Floor <nathan.floor@jembi.org>
2019-08-01 22:49:31 +0200

    DSD report: updated WHO query to remove inner join on obs so we pick encounters without obs correctly

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/DsdQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/DsdQueries.java
@@ -105,8 +105,7 @@ public class DsdQueries {
             + "SELECT e.patient_id, e.encounter_id, e.encounter_datetime "
             + "FROM patient p "
             + "INNER JOIN encounter e ON p.patient_id=e.patient_id "
-            + "INNER JOIN obs o ON e.encounter_id=o.encounter_id "
-            + "WHERE p.voided=0 AND e.voided=0 AND o.voided=0 "
+            + "WHERE p.voided=0 AND e.voided=0 "
             + "AND e.encounter_type IN ("
             + adultSeguimentoEncounter
             + ","
