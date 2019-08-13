2f5bdda75f7915e6e052db209902b29b6cbf9617
KelvinWanyama <kevmurumba@gmail.com>
2019-08-13 18:05:50 +0300

    EC20 check with both webview and excel report output.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupDataQualityReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupDataQualityReport.java
@@ -78,6 +78,8 @@ public class SetupDataQualityReport extends EptsDataExportManager {
 
   private Ec19PatientListDataset ec19PatientListDataset;
 
+  private Ec20PatientListDataset ec20PatientListDataset;
+
   private GetCustomConfigurationDataset getCustomConfigurationDataset;
 
   private HivMetadata hivMetadata;
@@ -105,6 +107,7 @@ public class SetupDataQualityReport extends EptsDataExportManager {
       Ec17PatientListDataset ec17PatientListDataset,
       Ec18PatientListDataset ec18PatientListDataset,
       Ec19PatientListDataset ec19PatientListDataset,
+      Ec20PatientListDataset ec20PatientListDataset,
       GetCustomConfigurationDataset getCustomConfigurationDataset,
       HivMetadata hivMetadata) {
     this.summaryDataQualityDataset = summaryDataQualityDataset;
@@ -128,6 +131,7 @@ public class SetupDataQualityReport extends EptsDataExportManager {
     this.ec17PatientListDataset = ec17PatientListDataset;
     this.ec18PatientListDataset = ec18PatientListDataset;
     this.ec19PatientListDataset = ec19PatientListDataset;
+    this.ec20PatientListDataset = ec20PatientListDataset;
     this.hivMetadata = hivMetadata;
     this.getCustomConfigurationDataset = getCustomConfigurationDataset;
   }
@@ -245,6 +249,10 @@ public class SetupDataQualityReport extends EptsDataExportManager {
         "EC19",
         Mapped.mapStraightThrough(
             ec19PatientListDataset.ec19PatientListDataset(getDataParameters())));
+    rd.addDataSetDefinition(
+        "EC20",
+        Mapped.mapStraightThrough(
+            ec20PatientListDataset.ec20PatientListDatset(getDataParameters())));
 
     rd.addDataSetDefinition(
         "EC01",
@@ -273,7 +281,7 @@ public class SetupDataQualityReport extends EptsDataExportManager {
       Properties props = new Properties();
       props.put(
           "repeatingSections",
-          "sheet:2,row:7,dataset:EC1 | sheet:3,row:7,dataset:EC2 | sheet:4,row:7,dataset:EC3 | sheet:5,row:7,dataset:EC4 | sheet:6,row:7,dataset:EC5 | sheet:7,row:7,dataset:EC6 | sheet:8,row:7,dataset:EC7 | sheet:9,row:7,dataset:EC8 | sheet:10,row:7,dataset:EC9 | sheet:11,row:7,dataset:EC10 | sheet:12,row:7,dataset:EC11 | sheet:13,row:7,dataset:EC12 | sheet:14,row:7,dataset:EC13 | sheet:15,row:7,dataset:EC14 | sheet:16,row:7,dataset:EC15 | sheet:17,row:7,dataset:EC16 | sheet:18,row:7,dataset:EC17 | sheet:19,row:7,dataset:EC18 | sheet:20,row:7,dataset:EC19");
+          "sheet:2,row:7,dataset:EC1 | sheet:3,row:7,dataset:EC2 | sheet:4,row:7,dataset:EC3 | sheet:5,row:7,dataset:EC4 | sheet:6,row:7,dataset:EC5 | sheet:7,row:7,dataset:EC6 | sheet:8,row:7,dataset:EC7 | sheet:9,row:7,dataset:EC8 | sheet:10,row:7,dataset:EC9 | sheet:11,row:7,dataset:EC10 | sheet:12,row:7,dataset:EC11 | sheet:13,row:7,dataset:EC12 | sheet:14,row:7,dataset:EC13 | sheet:15,row:7,dataset:EC14 | sheet:16,row:7,dataset:EC15 | sheet:17,row:7,dataset:EC16 | sheet:18,row:7,dataset:EC17 | sheet:19,row:7,dataset:EC18 | sheet:20,row:7,dataset:EC19 | sheet:21,row:7,dataset:EC20");
       props.put("sortWeight", "5000");
       reportDesign.setProperties(props);
     } catch (IOException e) {
