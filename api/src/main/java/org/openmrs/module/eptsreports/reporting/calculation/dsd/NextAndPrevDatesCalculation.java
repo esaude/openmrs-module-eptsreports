cf79ef564bc394726c0742a1b72977ec5847d20f
KelvinWanyama <kevmurumba@gmail.com>
2019-09-03 10:20:57 +0300

    Fixes to the NextAnPrevDatesCalculation: Excludes the obs with null dates.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/dsd/NextAndPrevDatesCalculation.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/dsd/NextAndPrevDatesCalculation.java
@@ -62,6 +62,8 @@ public class NextAndPrevDatesCalculation extends AbstractPatientCalculation {
       while (iterator.hasNext()) {
         Obs obs = iterator.next();
         if (obs.getVoided()
+                || obs.getValueDate() != null
+                || obs.getValueDatetime() != null
             || obs.getEncounter() == null
             || (obs.getEncounter() != null
                 && (obs.getEncounter().getEncounterId() != lastEncounter.getEncounterId()))) {
