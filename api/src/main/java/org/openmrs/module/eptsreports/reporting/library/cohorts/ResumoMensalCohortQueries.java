23a57b19738810065eca49cabf9a1af9c3f7441e
Yassin <yassin.nurmahomed@jembi.org>
2019-09-13 12:33:58 +0200

    Use reporting API with B8

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ResumoMensalCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ResumoMensalCohortQueries.java
@@ -244,17 +244,14 @@ public class ResumoMensalCohortQueries {
 
   /** @return B8: Number of died patients during the current month */
   public CohortDefinition getNumberOfPatientsWhoDiedDuringCurrentMonthB8() {
-    SqlCohortDefinition cd = new SqlCohortDefinition();
+    EncounterWithCodedObsCohortDefinition cd = new EncounterWithCodedObsCohortDefinition();
     cd.setName("Number of patients transferred out during the current month");
-    cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
-    cd.addParameter(new Parameter("endDate", "End Date", Date.class));
-    cd.addParameter(new Parameter("location", "Location", Location.class));
-    cd.setQuery(
-        ResumoMensalQueries.getPatientsWhoDiedDuringCurrentMonth(
-            hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
-            hivMetadata.getStateOfStayOfArtPatient().getConceptId(),
-            hivMetadata.getPatientHasDiedConcept().getConceptId()));
-
+    cd.addParameter(new Parameter("onOrAfter", "onOrAfter", Date.class));
+    cd.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+    cd.addParameter(new Parameter("locationList", "location", Location.class));
+    cd.addEncounterType(hivMetadata.getAdultoSeguimentoEncounterType());
+    cd.setConcept(hivMetadata.getStateOfStayOfArtPatient());
+    cd.addIncludeCodedValue(hivMetadata.getPatientHasDiedConcept());
     return cd;
   }
 
--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/resumo/ResumoMensalDataSetDefinition.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/resumo/ResumoMensalDataSetDefinition.java
@@ -473,10 +473,10 @@ public class ResumoMensalDataSetDefinition extends BaseDataSet {
 
   private Mapped<CohortIndicator> getPatientsWhoDiedDuringCurrentMonth() {
     String name = "Patients who died during the current month";
-    Mapped<CohortDefinition> cohort =
-        mapStraightThrough(
-            resumoMensalCohortQueries.getNumberOfPatientsWhoDiedDuringCurrentMonthB8());
-    return mapStraightThrough(eptsGeneralIndicator.getIndicator(name, cohort));
+    String mappings = "onOrAfter=${startDate},onOrBefore=${endDate},locationList=${location}";
+    CohortDefinition cohort =
+        resumoMensalCohortQueries.getNumberOfPatientsWhoDiedDuringCurrentMonthB8();
+    return mapStraightThrough(eptsGeneralIndicator.getIndicator(name, map(cohort, mappings)));
   }
 
   private Mapped<CohortIndicator> getPatientsWhoStartedArtByEndOfPreviousMonth() {
