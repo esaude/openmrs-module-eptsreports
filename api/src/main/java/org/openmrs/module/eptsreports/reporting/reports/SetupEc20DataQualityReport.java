29041c8fd5f2345c1123ada14b2e0c0d64ccaa9e
Nathan Floor <nathan.floor@jembi.org>
2019-10-14 10:50:02 +0200

    MEPTS-266: updated dead concept for txML and disabled EC20 Data quality report

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupEc20DataQualityReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupEc20DataQualityReport.java
@@ -22,6 +22,7 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Deprecated
 @Component
 public class SetupEc20DataQualityReport extends EptsDataExportManager {
 
