5b35e7ded6dc0274be9506a48b3880c5beb4dc8e
cfaife <clerio.faife@gmail.com>
2019-11-08 12:52:32 +0200

    MEPTS-284  moved the LTFU for  genericCohortQueries

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/GenderCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/GenderCohortQueries.java
@@ -13,12 +13,22 @@
  */
 package org.openmrs.module.eptsreports.reporting.library.cohorts;
 
+import java.util.Date;
+
+import org.openmrs.Location;
+import org.openmrs.module.eptsreports.reporting.library.queries.TXCurrQueries;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CompositionCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.GenderCohortDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GenderCohortQueries {
+	
+	@Autowired private TxCurrCohortQueries txCurrCohortQueries;
 
   /**
    * Patients who are female
@@ -44,5 +54,32 @@ public class GenderCohortQueries {
     cohort.setMaleIncluded(true);
     cohort.setFemaleIncluded(false);
     return cohort;
+  }
+  
+  public CohortDefinition getPatinetWhoToLostToFollowUp() {
+	  CompositionCohortDefinition  definition  = new CompositionCohortDefinition();
+	  
+	  
+	    String mappings = "onOrBefore=${onOrBefore},location=${location}";
+	    
+	    definition.addSearch(
+	            "31",
+	            EptsReportUtils.map(txCurrCohortQueries.
+	            		getPatientHavingLastScheduledDrugPickupDate(),
+	                mappings));
+	    
+	    definition.addSearch(
+	            "32",
+	            EptsReportUtils.map(txCurrCohortQueries.
+	            		getPatientWithoutScheduledDrugPickupDateMasterCardAmdArtPickup(),
+	                mappings));
+
+	  
+	  definition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+	  definition.addParameter(new Parameter("location", "location", Location.class));
+	  definition.setCompositionString(
+		        "31 OR  32");
+	  
+	  return definition;
   }
 }
