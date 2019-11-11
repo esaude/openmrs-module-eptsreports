126d7a18aab817f727deb9fa271b744eef421ce4
cfaife <clerio.faife@gmail.com>
2019-11-11 10:34:20 +0200

    MEPTS-284: removed unsed  variable

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/GenderCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/GenderCohortQueries.java
@@ -15,13 +15,10 @@ package org.openmrs.module.eptsreports.reporting.library.cohorts;
 
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.GenderCohortDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GenderCohortQueries {
-
-  @Autowired private TxCurrCohortQueries txCurrCohortQueries;
 
   /**
    * Patients who are female
