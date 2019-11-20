8710f0a068ab6efb78a795e66aa761129d051731
KelvinWanyama <kevmurumba@gmail.com>
2019-11-20 14:46:30 +0300

    Added the missing concept ID iin the configurations and Cohort definition for TB query.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/HivCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/HivCohortQueries.java
@@ -20,6 +20,7 @@ import org.openmrs.Program;
 import org.openmrs.ProgramWorkflowState;
 import org.openmrs.module.eptsreports.metadata.CommonMetadata;
 import org.openmrs.module.eptsreports.metadata.HivMetadata;
+import org.openmrs.module.eptsreports.reporting.library.queries.HivQueries;
 import org.openmrs.module.eptsreports.reporting.library.queries.ViralLoadQueries;
 import org.openmrs.module.reporting.cohort.definition.CodedObsCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
@@ -337,100 +338,25 @@ public class HivCohortQueries {
     return cd;
   }
 
-  private CohortDefinition getPatientsOnTbTreatment(
-      int adultoSeguimentoEncounterTypeId,
-      int arvPediatriaSeguimentoEncounterTypeId,
-      int tbStartDateConceptId,
-      int tbEndDateConceptId,
-      int tbProgramId,
-      int patientStateId,
-      int activeTBConceptId,
-      int yesConceptId,
-      int tbTreatmentPlanConceptId,
-      int startDrugsConceptId,
-      int continueRegimenConceptId) {
-
+  private CohortDefinition getPatientsOnTbTreatment() {
     SqlCohortDefinition cd = new SqlCohortDefinition();
     cd.setName("patientsOnTbTreatment");
     cd.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
     cd.addParameter(new Parameter("location", "location", Location.class));
 
-    String query =
-        " SELECT p.patient_id FROM patient p "
-            + "JOIN encounter e ON p.patient_id=e.patient_id "
-            + "JOIN obs o ON p.patient_id=o.person_id "
-            + "WHERE e.encounter_type IN ( "
-            + adultoSeguimentoEncounterTypeId
-            + ","
-            + arvPediatriaSeguimentoEncounterTypeId
-            + " ) AND p.voided=0 AND e.voided=0 AND o.concept_id= "
-            + tbStartDateConceptId
-            + " AND o.value_datetime BETWEEN :startDate AND :endDate AND e.location_id=:location "
-            + "UNION "
-            + "SELECT p.patient_id FROM patient p "
-            + "JOIN encounter e ON p.patient_id=e.patient_id "
-            + "JOIN obs o ON p.patient_id=o.person_id "
-            + "WHERE e.encounter_type IN ( "
-            + adultoSeguimentoEncounterTypeId
-            + ","
-            + arvPediatriaSeguimentoEncounterTypeId
-            + " ) AND p.voided=0 AND e.voided=0 AND o.concept_id= "
-            + tbEndDateConceptId
-            + " AND o.value_datetime IS NULL AND e.location_id=:location "
-            + "UNION "
-            + "SELECT p.patient_id FROM patient p "
-            + "JOIN encounter e ON p.patient_id=e.patient_id "
-            + "JOIN obs o ON p.patient_id=o.person_id "
-            + "WHERE e.encounter_type IN ( "
-            + adultoSeguimentoEncounterTypeId
-            + ","
-            + arvPediatriaSeguimentoEncounterTypeId
-            + " ) AND p.voided=0 AND e.voided=0 AND o.concept_id= "
-            + tbEndDateConceptId
-            + " AND o.value_datetime > :endDate AND e.location_id=:location "
-            + "UNION "
-            + "SELECT p.patient_id FROM patient p "
-            + "JOIN patient_program pp ON p.patient_id=pp.patient_id "
-            + "JOIN patient_state ps ON pp.patient_program_id=ps.patient_program_id "
-            + "WHERE pp.program_id= "
-            + tbProgramId
-            + " AND ps.patient_state_id= "
-            + patientStateId
-            + " AND e.location_id=:location "
-            + "UNION "
-            + "SELECT p.patient_id FROM patient p "
-            + "JOIN encounter e ON p.patient_id=e.patient_id "
-            + "JOIN obs o ON p.patient_id=o.person_id "
-            + "WHERE e.encounter_type IN ( "
-            + adultoSeguimentoEncounterTypeId
-            + ","
-            + arvPediatriaSeguimentoEncounterTypeId
-            + " ) AND p.voided=0 AND e.voided=0 "
-            + "AND o.concept_id= "
-            + activeTBConceptId
-            + " AND o.value_coded= "
-            + yesConceptId
-            + "AND e.encounter_datetime BETWEEN :endDate AND DATE_SUB(:endDate, INTERVAL 6 MONTH) AND e.location_id=:location "
-            + "UNION "
-            + "SELECT p.patient_id FROM patient p "
-            + "JOIN encounter e ON p.patient_id=e.patient_id "
-            + "JOIN obs o ON p.patient_id=o.person_id "
-            + "WHERE e.encounter_type IN ( "
-            + adultoSeguimentoEncounterTypeId
-            + ","
-            + arvPediatriaSeguimentoEncounterTypeId
-            + " ) AND p.voided=0 AND e.voided=0 "
-            + "AND o.concept_id= "
-            + tbTreatmentPlanConceptId
-            + " AND o.value_coded IN ( "
-            + startDrugsConceptId
-            + ","
-            + continueRegimenConceptId
-            + " ) "
-            + "AND o.value_datetime BETWEEN :startDate AND :endDate AND e.location_id=:location ";
-
-    cd.setQuery(
-        String.format(query, hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId()));
+    cd.setQuery(HivQueries.getPatientsOnTbTreatmentQuery(
+            hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
+            hivMetadata.getARVPediatriaSeguimentoEncounterType().getEncounterTypeId(),
+            hivMetadata.getTBDrugStartDateConcept().getConceptId(),
+            hivMetadata.getTBDrugEndDateConcept().getConceptId(),
+            hivMetadata.getTBProgram().getProgramId(),
+            6269,
+            hivMetadata.getActiveTBConcept().getConceptId(),
+            hivMetadata.getYesConcept().getConceptId(),
+            hivMetadata.getTBTreatmentPlanConcept().getConceptId(),
+            hivMetadata.getStartDrugs().getConceptId(),
+            hivMetadata.getContinueRegimen().getConceptId()
+    ));
 
     return cd;
   }
