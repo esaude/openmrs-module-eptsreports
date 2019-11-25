53ced2f6cf58d9c72d0a53400877a018eeb59395
cfaife <clerio.faife@gmail.com>
2019-11-25 11:38:48 +0200

    MEPTS-268 formated the code

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/EriDSDDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/EriDSDDataset.java
@@ -512,12 +512,10 @@ public class EriDSDDataset extends BaseDataSet {
   }
 
   private List<ColumnParameters> getChildrenColumn() {
-	  ColumnParameters twoTo4 = new ColumnParameters("twoTo4", "2-4", "age=2-4", "01");
-	    ColumnParameters fiveTo9 = new ColumnParameters("fiveTo9", "5-9", "age=5-9", "02");
-	    ColumnParameters tenTo14 = new ColumnParameters("tenTo14", "10-14", "age=10-14", "03");
-	    ColumnParameters zeroTo2 = new ColumnParameters("zeroTo2", "<2", "age=<2", "04");
-    return Arrays.asList(twoTo4, fiveTo9, tenTo14,zeroTo2);
+    ColumnParameters twoTo4 = new ColumnParameters("twoTo4", "2-4", "age=2-4", "01");
+    ColumnParameters fiveTo9 = new ColumnParameters("fiveTo9", "5-9", "age=5-9", "02");
+    ColumnParameters tenTo14 = new ColumnParameters("tenTo14", "10-14", "age=10-14", "03");
+    ColumnParameters zeroTo2 = new ColumnParameters("zeroTo2", "<2", "age=<2", "04");
+    return Arrays.asList(twoTo4, fiveTo9, tenTo14, zeroTo2);
   }
-
-  
 }
