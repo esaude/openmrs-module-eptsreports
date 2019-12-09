ad39b6a3fbfd7357654926673509d659cdb82571
KelvinWanyama <kevmurumba@gmail.com>
2019-12-09 14:17:56 +0300

    ML Report setup

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERQuartelyML.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERQuartelyML.java
@@ -1,8 +1,14 @@
 package org.openmrs.module.eptsreports.reporting.reports;
 
-import org.openmrs.module.eptsreports.reporting.library.datasets.EriDSDDataset;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.datasets.TxMlDataset;
 import org.openmrs.module.eptsreports.reporting.library.datasets.TxNewDataset;
 import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.ReportingException;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.report.ReportDesign;
@@ -10,73 +16,72 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
-
 @Component
 public class SetupMERQuartelyML extends EptsDataExportManager {
-    @Autowired private TxNewDataset txNewDataset;
-    @Autowired private EriDSDDataset eriDSDDataset;
+  @Autowired private TxNewDataset txNewDataset;
+  @Autowired private TxMlDataset txMlDataset;
+  @Autowired private GenericCohortQueries genericCohortQueries;
 
-    @Override
-    public String getExcelDesignUuid() {
-        return "026a9ca6-17f7-11ea-be6d-3b6880ec2f6f";
+  @Override
+  public String getExcelDesignUuid() {
+    return "026a9ca6-17f7-11ea-be6d-3b6880ec2f6f";
+  }
+
+  @Override
+  public String getUuid() {
+    return "0ed94726-17f7-11ea-9717-fb550fc33883";
+  }
+
+  @Override
+  public String getName() {
+    return "PEPFAR MER 2.3 Quarterly ML";
+  }
+
+  @Override
+  public String getDescription() {
+    return "MER Quarterly ML Report";
+  }
+
+  @Override
+  public ReportDefinition constructReportDefinition() {
+    ReportDefinition reportDefinition = new ReportDefinition();
+    reportDefinition.setUuid(getUuid());
+    reportDefinition.setName(getName());
+    reportDefinition.setDescription(getDescription());
+    reportDefinition.setParameters(txNewDataset.getParameters());
+
+    reportDefinition.addDataSetDefinition(
+        "ML", Mapped.mapStraightThrough(txMlDataset.constructtxMlDataset()));
+    reportDefinition.setBaseCohortDefinition(
+        EptsReportUtils.map(
+            genericCohortQueries.getBaseCohort(), "endDate=${endDate},location=${location}"));
+
+    return reportDefinition;
+  }
+
+  @Override
+  public String getVersion() {
+    return "1.0-SNAPSHOT";
+  }
+
+  @Override
+  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+    ReportDesign reportDesign = null;
+    try {
+      reportDesign =
+          createXlsReportDesign(
+              reportDefinition,
+              "PEPFAR_MER_2.4_QUARTERLY_ML.xls",
+              "PEPFAR MER 2.4 Quarterly ML Report",
+              getExcelDesignUuid(),
+              null);
+      Properties props = new Properties();
+      props.put("sortWeight", "5000");
+      reportDesign.setProperties(props);
+    } catch (IOException e) {
+      throw new ReportingException(e.toString());
     }
 
-    @Override
-    public String getUuid() {
-        return "0ed94726-17f7-11ea-9717-fb550fc33883";
-    }
-
-    @Override
-    public String getName() {
-        return "PEPFAR MER 2.3 Quarterly ML";
-    }
-
-    @Override
-    public String getDescription() {
-        return "MER Quarterly ML Report";
-    }
-
-    @Override
-    public ReportDefinition constructReportDefinition() {
-        ReportDefinition reportDefinition = new ReportDefinition();
-        reportDefinition.setUuid(getUuid());
-        reportDefinition.setName(getName());
-        reportDefinition.setDescription(getDescription());
-        reportDefinition.setParameters(txNewDataset.getParameters());
-
-        //TODO replace eriDSDDatset with a new dataset
-        reportDefinition.addDataSetDefinition("ERIDSD-ML", Mapped.mapStraightThrough(eriDSDDataset.constructEriDSDDataset()));
-
-        return reportDefinition;
-    }
-
-    @Override
-    public String getVersion() {
-        return "1.0-SNAPSHOT";
-    }
-
-    @Override
-    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-        ReportDesign reportDesign = null;
-        try {
-            reportDesign =
-                    createXlsReportDesign(
-                            reportDefinition,
-                            "PEPFAR_MER_2.4_QUARTERLY_ML.xls",
-                            "PEPFAR MER 2.4 Quarterly ML Report",
-                            getExcelDesignUuid(),
-                            null);
-            Properties props = new Properties();
-            props.put("sortWeight", "5000");
-            reportDesign.setProperties(props);
-        } catch (IOException e) {
-            throw new ReportingException(e.toString());
-        }
-
-        return Arrays.asList(reportDesign);
-    }
+    return Arrays.asList(reportDesign);
+  }
 }
