6924f1bc4967cf510223535c2b00b8754b954dc8
Kelvin Murumba <kevmurumba@gmail.com>
2019-12-11 10:01:12 +0300

    Mepts 287 updates dsd indicators d1 (#153)
    * Removed criteria:WHO stage III and IV
    
    * Removed the WHO stages
    
    * Rename N3 to N4
    
    * Rename N2 to N3
    
    * Fix names
    
    * Rename N1 to N2
    
    * Add new indicator to template
    
    * Added the Patients on Sarcoma karposi query
    
    * Excluded the patients with Sarcoma Karposi from TX-Curr
    
    * Remove breastfeeding and pregnant disag
    
    * Refactor
    
    * Removed the WHO stage 3 and 4 criteria in D1
    
    * Add new criterias
    
    * Change startDate of D1 criteria e to Last 6 months from end of reporting period
    
    * Update breastfeeding cohort
    
    * Additional concepts to the configurations for D1 condition e (iii)
    
    * Add <2 age disag
    
    * Added concepts for D1 criteria e
    
    * Added the TB query template
    
    * Added HIVQueries class to hold the Common queries.
    
    * Added the missing concept ID iin the configurations and Cohort definition for TB query.
    
    * Added comments and TODO reminders
    
    * Added Patient On TB Treatment Criteria.
    
    * Made changes to the patients who are enrolled in TB Program with enrollment Date query
    
    * Added the activeOnProgram Concept to the Cohort queries.
    
    * Removed TODO on Patients On TB Treatment CD
    
    * Updated the comments on the TB Patients query.
    
    * Made changes to the query to pick patient state id
    
    * format
    
    * Changed the CD4 Time modifier from Any to Last.
    
    * Made changes to the patientWithViralLoadLessThan1000 query
    
    * Added Patients with The Recent VL encounter query.
    
    * Added concept UUIDs in the configs and made changes to the getPatientsWithViralLoadLessThan100 CD.
    
    * Patients with Recent VL Encounter CD
    
    * Added Patients with Viral Load less than 1000 composition string
    
    * Matched the parameters to the SQL query.
    
    * Added the Active On Program state from TB state workflow
    
    * Made changes to the TB SQL query
    
    * Made changes to the SQL query: pick the last encounter  based on obs_datetime and encounter_datetime
    
    * Added the encounter type id in the inner queries.
    
    * Made changes to Breastfeeding: Reusing the TXNew query
    
    * Updated the breastfeeding criteria in D1
    
    * Format D1 composition
    
    * Format for the VL less than 1000 query
    
    * Format Patients with VL encounter query
    
    * Removed breatfeeding and pregnant from Not eligible
    
    * Changed the pregnant query.
    
    * Added a new CD without patients on TB
    
    * Added a new CD Patients who are eligible D1
    
    * Updated the D1 eligible disaggregations.
    
    * Made changes to the Concept configurattions.
    
    * Format
    
    * Removed the CD, to reuse getAllPatientsWhoAreActiveAndStable
    
    * Reused D1
    
    * Removed duplication in metdadata configuration

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/HivQueries.java
@@ -0,0 +1,113 @@
+package org.openmrs.module.eptsreports.reporting.library.queries;
+
+public class HivQueries {
+
+  /**
+   * Patients on TB Treatment
+   *
+   * @param adultoSeguimentoEncounterTypeId
+   * @param arvPediatriaSeguimentoEncounterTypeId
+   * @param tbStartDateConceptId
+   * @param tbEndDateConceptId
+   * @param tbProgramId
+   * @param patientStateId
+   * @param activeTBConceptId
+   * @param yesConceptId
+   * @param tbTreatmentPlanConceptId
+   * @param startDrugsConceptId
+   * @param continueRegimenConceptId
+   * @return
+   */
+  public static String getPatientsOnTbTreatmentQuery(
+      int adultoSeguimentoEncounterTypeId,
+      int arvPediatriaSeguimentoEncounterTypeId,
+      int tbStartDateConceptId,
+      int tbEndDateConceptId,
+      int tbProgramId,
+      int patientStateId,
+      int activeTBConceptId,
+      int yesConceptId,
+      int tbTreatmentPlanConceptId,
+      int startDrugsConceptId,
+      int continueRegimenConceptId) {
+
+    String query =
+        "SELECT p.patient_id "
+            + "FROM patient p "
+            + "JOIN encounter e ON p.patient_id = e.patient_id "
+            + "JOIN obs start ON e.encounter_id = start.encounter_id "
+            + "WHERE  e.encounter_type IN (%d,%d) AND start.concept_id = %d "
+            + "AND start.value_datetime is NOT NULL "
+            + "AND start.value_datetime BETWEEN :startDate AND :endDate "
+            + "AND e.location_id=:location "
+            + "AND p.patient_id NOT IN "
+            + "( "
+            + "SELECT p1.patient_id "
+            + "FROM patient p1 "
+            + "JOIN encounter e1 ON p1.patient_id = e1.patient_id "
+            + "JOIN obs end ON e1.encounter_id = end.encounter_id "
+            + "WHERE  e1.encounter_type IN (%d,%d) AND end.concept_id = %d "
+            + "AND (end.value_datetime is NOT NULL "
+            + "AND end.value_datetime <=  :endDate ) "
+            + "AND  end.value_datetime > start.value_datetime "
+            + "AND e.location_id=:location "
+            + ") "
+            + "UNION "
+            + "SELECT p.patient_id FROM patient p  "
+            + "JOIN encounter e ON p.patient_id=e.patient_id "
+            + "JOIN patient_program pp ON p.patient_id=pp.patient_id "
+            + "JOIN patient_state ps ON pp.patient_program_id=ps.patient_program_id  "
+            + "WHERE pp.program_id=%d AND ps.state=%d AND e.location_id=:location AND ps.start_date BETWEEN :startDate AND :endDate AND "
+            + "(ps.end_date IS NULL OR ps.end_date > :endDate )AND p.voided=0 AND e.voided=0 "
+            + "UNION "
+            + "SELECT e.patient_id "
+            + "FROM encounter e "
+            + "JOIN obs o ON e.encounter_id = o.encounter_id "
+            + "JOIN (SELECT p.patient_id, max(e.encounter_datetime) encounter_datetime "
+            + "FROM patient p "
+            + " JOIN encounter e ON p.patient_id = e.patient_id "
+            + "JOIN obs o ON e.encounter_id = o.encounter_id "
+            + "WHERE o.concept_id = %d "
+            + "AND e.location_id = :location "
+            + "AND e.encounter_type IN (%d,%d) "
+            + "AND e.encounter_datetime BETWEEN date_sub(e.encounter_datetime, INTERVAL 6 MONTH) AND :endDate AND p.voided=0 "
+            + "GROUP BY p.patient_id) last "
+            + "ON e.patient_id = last.patient_id AND e.encounter_datetime = last.encounter_datetime "
+            + "WHERE o.value_coded = %d AND e.voided=0 AND o.voided=0 "
+            + "UNION "
+            + "SELECT e.patient_id "
+            + "FROM encounter e "
+            + "JOIN obs o ON e.encounter_id = o.encounter_id "
+            + "JOIN (SELECT p.patient_id, max(o.value_datetime) value_datetime "
+            + "FROM patient p "
+            + "JOIN encounter e ON p.patient_id = e.patient_id "
+            + "JOIN obs o ON e.encounter_id = o.encounter_id "
+            + "WHERE o.concept_id = %d "
+            + "AND e.location_id = :location "
+            + "AND e.encounter_type IN (%d,%d) "
+            + "AND o.value_datetime BETWEEN :startDate AND :endDate AND p.voided=0 "
+            + "GROUP BY p.patient_id) last "
+            + "ON e.patient_id = last.patient_id AND o.value_datetime = last.value_datetime "
+            + "WHERE o.value_coded IN(%d,%d) AND e.voided=0 AND o.voided=0 ";
+
+    return String.format(
+        query,
+        adultoSeguimentoEncounterTypeId,
+        arvPediatriaSeguimentoEncounterTypeId,
+        tbStartDateConceptId,
+        adultoSeguimentoEncounterTypeId,
+        arvPediatriaSeguimentoEncounterTypeId,
+        tbEndDateConceptId,
+        tbProgramId,
+        patientStateId,
+        activeTBConceptId,
+        adultoSeguimentoEncounterTypeId,
+        arvPediatriaSeguimentoEncounterTypeId,
+        yesConceptId,
+        tbTreatmentPlanConceptId,
+        adultoSeguimentoEncounterTypeId,
+        arvPediatriaSeguimentoEncounterTypeId,
+        startDrugsConceptId,
+        continueRegimenConceptId);
+  }
+}
