19df3bfde422768b3ea9183c7a56d91aff33561f
Nathan Floor <nathan.floor@jembi.org>
2019-12-17 16:04:57 +0200

    MEPTS-307: RTT changes (#169)
    * MEPTS-307: combined previous 2 queries for RTT into single SQL query to match updated specs.
    
    * MEPTS-307: removed unused RTT cohorts & querys
    
    * MEPTS-307: removed composition cohort since no longer needed

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TxRttQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TxRttQueries.java
@@ -16,107 +16,95 @@ package org.openmrs.module.eptsreports.reporting.library.queries;
 public class TxRttQueries {
 
   /**
-   * For each occurred consultation (Ficha de Seguimento Adulto, Pediatria or Ficha Clinica) of
-   * Encounter Type Ids 6 or 9 during the reporting period, the occurred encounter date minus the
-   * previous scheduled consultation date (Concept ID 1410 value_datetime) from the previous
-   * consultation of Encounter Type Ids 6 or 9, is greater than 28 days
+   * Check each encounter against the previous scheduled date of drug pickup AND consultations to be
+   * greater than 28 days. Pick any of the encounters which occurred 28 dyas after the previously
+   * scheduled consultation or drug pickup.
    *
+   * @param adultoSegEncounter
+   * @param pediatriaSegEncounter
+   * @param pharmacyEncounter
+   * @param masterCardEncounter
+   * @param consultationConcept
+   * @param nextDrugPickupConcept
+   * @param dateOfArtPickupConcept
    * @return String
    */
-  public static String getPatientsHavingConsultationAfter28DaysPriorToPreviousConsultation(
-      int adultoSeg, int padiatSeg, int conceptid) {
+  public static String getAllPatientsWhoMissedPreviousAppointmentBy28Days(
+      int adultoSegEncounter,
+      int pediatriaSegEncounter,
+      int pharmacyEncounter,
+      int masterCardEncounter,
+      int consultationConcept,
+      int nextDrugPickupConcept,
+      int dateOfArtPickupConcept) {
     String query =
-        "SELECT e.patient_id "
-            + " FROM ("
-            + " SELECT e.encounter_id,  p.patient_id, "
-            + " ("
-            + " SELECT oo.value_datetime "
-            + " FROM encounter ee "
-            + " INNER JOIN obs oo ON ee.encounter_id = oo.encounter_id "
+        "SELECT e.patient_id FROM "
+            + " (SELECT"
+            + "     e.encounter_id, "
+            + "     p.patient_id, "
+            + "     ("
+            + "       SELECT oo.value_datetime "
+            + "       FROM encounter ee "
+            + "         INNER JOIN obs oo ON "
+            + "           ee.encounter_id = oo.encounter_id "
+            + "       WHERE "
+            + "         ee.voided = 0 "
+            + "         AND oo.voided = 0 "
+            + "         AND ee.encounter_type IN (%d,%d) "
+            + "         AND oo.concept_id = %d "
+            + "         AND ee.patient_id = e.patient_id "
+            + "         AND ee.encounter_datetime < e.encounter_datetime "
+            + "         AND ee.location_id = :location "
+            + "         AND ee.encounter_datetime <= :endDate "
+            + "       ORDER BY ee.encounter_datetime DESC "
+            + "       LIMIT 1) AS prev_consultation_scheduled_date, "
+            + "     (SELECT "
+            + "       CASE WHEN ee.encounter_type = %d THEN oo.value_datetime "
+            + "       ELSE DATE_ADD(oo.value_datetime,"
+            + "       INTERVAL 30 DAY) END AS next_schedule_date "
+            + "     FROM encounter ee "
+            + "       INNER JOIN obs oo ON "
+            + "         ee.encounter_id = oo.encounter_id "
+            + "     WHERE "
+            + "       ee.voided = 0 "
+            + "       AND oo.voided = 0 "
+            + "       AND ( "
+            + "         (ee.encounter_type = %d	AND oo.concept_id = %d) "
+            + "         OR (ee.encounter_type = %d AND oo.concept_id = %d) "
+            + "       )"
+            + "       AND ee.patient_id = e.patient_id "
+            + "       AND ee.encounter_datetime < e.encounter_datetime "
+            + "       AND ee.location_id = :location "
+            + "       AND ee.encounter_datetime <= :endDate "
+            + "     ORDER BY "
+            + "       ee.encounter_datetime DESC "
+            + "     LIMIT 1 ) AS prev_drug_pickup_scheduled_date, "
+            + "     e.encounter_datetime "
+            + "   FROM patient p "
+            + "     INNER JOIN encounter e ON "
+            + "       e.patient_id = p.patient_id "
+            + "   WHERE "
+            + "     p.voided = 0 "
+            + "     AND e.voided = 0 "
+            + "     AND e.encounter_type IN (%d,%d,%d,%d) "
+            + "     AND e.location_id = :location ) e "
             + " WHERE "
-            + " ee.voided = 0 AND "
-            + " oo.voided = 0 AND "
-            + " ee.encounter_type IN (%d, %d) AND "
-            + " oo.concept_id = %d AND "
-            + " ee.patient_id = e.patient_id AND "
-            + " ee.encounter_datetime < e.encounter_datetime AND "
-            + " ee.location_id = :location AND "
-            + " ee.encounter_datetime <= :endDate "
-            + " ORDER BY ee.encounter_datetime DESC LIMIT 1) AS prev_scheduled_date, e.encounter_datetime "
-            + " FROM	patient p "
-            + " INNER JOIN encounter e ON e.patient_id = p.patient_id "
-            + " WHERE "
-            + " p.voided = 0 AND "
-            + " e.voided = 0 AND "
-            + " e.encounter_type IN (%d, %d) AND "
-            + " e.location_id = :location "
-            + " ) e "
-            + " WHERE "
-            + " TIMESTAMPDIFF(DAY, e.prev_scheduled_date, e.encounter_datetime) > 28 "
-            + " AND e.encounter_datetime BETWEEN :startDate AND :endDate ";
-
-    return String.format(query, adultoSeg, padiatSeg, conceptid, adultoSeg, padiatSeg);
-  }
-
-  /**
-   * For each occurred drug pick ups of Encounter Types Ids 18 or 52 during the reporting period,
-   * the occurred encounter date minus the previous scheduled drug pick up date (as the most recent
-   * between previous encounter type 18 scheduled date (Concept ID 1410 value_datetime) and the
-   * previous encounter type 52 pick up date (Concept ID 23866 value_datetime +30 days)), is greater
-   * than 28 days
-   *
-   * @return String
-   */
-  public static String
-      getAllPatientsWhoMissedDrugPickupHavingPreviousMasterCardAppointment30DaysWhichIs28DaysLaterThanEncounterDate(
-          int pharmacyEncounter,
-          int masterCardEncounter,
-          int nextDrugPickupConcept,
-          int dateOfArtPickupConcept) {
-    String query =
-        " SELECT e.patient_id "
-            + " FROM ("
-            + " SELECT e.encounter_id, p.patient_id,("
-            + " SELECT "
-            + " CASE WHEN ee.encounter_type = %d THEN "
-            + " oo.value_datetime "
-            + " ELSE "
-            + " DATE_ADD(oo.value_datetime, INTERVAL 30 DAY) "
-            + " END AS next_schedule_date "
-            + " FROM encounter ee "
-            + " INNER JOIN obs oo ON ee.encounter_id = oo.encounter_id "
-            + " WHERE "
-            + " ee.voided = 0 AND "
-            + " oo.voided = 0 AND "
-            + "("
-            + " (ee.encounter_type = %d AND oo.concept_id = %d) OR "
-            + " (ee.encounter_type = %d AND oo.concept_id = %d) "
-            + ") AND "
-            + " ee.patient_id = e.patient_id AND "
-            + " ee.encounter_datetime < e.encounter_datetime AND "
-            + " ee.location_id = :location AND "
-            + " ee.encounter_datetime <= :endDate "
-            + " ORDER BY ee.encounter_datetime DESC LIMIT 1 "
-            + ") AS prev_scheduled_date, "
-            + " e.encounter_datetime "
-            + " FROM patient p "
-            + " INNER JOIN encounter e ON e.patient_id = p.patient_id "
-            + " WHERE "
-            + " p.voided = 0 AND "
-            + " e.voided = 0 AND "
-            + " e.encounter_type IN (%d, %d) AND "
-            + " e.location_id = :location "
-            + ") e "
-            + " WHERE "
-            + " TIMESTAMPDIFF(DAY, e.prev_scheduled_date, e.encounter_datetime) > 28 "
-            + " AND e.encounter_datetime BETWEEN :startDate AND :endDate";
+            + "   TIMESTAMPDIFF(DAY,e.prev_consultation_scheduled_date,e.encounter_datetime) > 28 AND "
+            + "   TIMESTAMPDIFF(DAY,e.prev_drug_pickup_scheduled_date,e.encounter_datetime) > 28 AND "
+            + "   e.encounter_datetime BETWEEN :startDate AND :endDate "
+            + " GROUP BY patient_id";
     return String.format(
         query,
+        adultoSegEncounter,
+        pediatriaSegEncounter,
+        consultationConcept,
         pharmacyEncounter,
         pharmacyEncounter,
         nextDrugPickupConcept,
         masterCardEncounter,
         dateOfArtPickupConcept,
+        adultoSegEncounter,
+        pediatriaSegEncounter,
         pharmacyEncounter,
         masterCardEncounter);
   }
