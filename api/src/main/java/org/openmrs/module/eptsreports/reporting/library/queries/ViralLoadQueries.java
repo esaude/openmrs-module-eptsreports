30dbdea52458539436715524c7aa3a1e772a49ec
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2019-12-18 09:29:42 +0300

    Correct query errorin the union criteria

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/ViralLoadQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/ViralLoadQueries.java
@@ -80,7 +80,7 @@ public class ViralLoadQueries {
             + " obs o ON e.encounter_id=o.encounter_id "
             + " WHERE p.voided=0 AND e.voided=0 AND o.voided=0 AND "
             + " e.encounter_type IN (%d) AND o.concept_id=%d AND o.value_numeric IS NOT NULL AND "
-            + " o.obs_datetime BETWEEN date_add(date_add(:endDate, interval -12 MONTH), interval 1 day) AND :endDate "
+            + " o.obs_datetime BETWEEN date_add(date_add(:endDate, interval -12 MONTH), interval 1 day) AND :endDate AND "
             + " e.location_id=:location ";
     return String.format(
         query,
