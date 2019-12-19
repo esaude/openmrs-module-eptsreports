bd6404c1e4716eacb9fe7191d864984097a4d071
KelvinWanyama <kevmurumba@gmail.com>
2019-12-19 10:23:48 +0300

    MEPST-309: Added Test shel for the StartedArtOnLastClinicalContactCalculation

---
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/unit/calculation/txml/StartedArtOnLastClinicalContactCalculationTest.java
@@ -0,0 +1,52 @@
+package org.openmrs.module.eptsreports.reporting.unit.calculation.txml;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.openmrs.api.context.Context;
+import org.openmrs.calculation.patient.PatientCalculationService;
+import org.openmrs.calculation.patient.PatientCalculationServiceImpl;
+import org.openmrs.module.eptsreports.metadata.HivMetadata;
+import org.openmrs.module.eptsreports.reporting.calculation.common.EPTSCalculationService;
+import org.openmrs.module.eptsreports.reporting.calculation.txml.StartedArtOnLastClinicalContactCalculation;
+import org.openmrs.module.eptsreports.reporting.helper.TestsHelper;
+import org.openmrs.module.eptsreports.reporting.unit.PowerMockBaseContextTest;
+import org.openmrs.module.eptsreports.reporting.utils.EptsCalculationUtils;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
+@PrepareForTest(EptsCalculationUtils.class)
+public class StartedArtOnLastClinicalContactCalculationTest extends PowerMockBaseContextTest {
+  @Mock private HivMetadata hivMetadata;
+  @Mock private EPTSCalculationService eptsCalculationService;
+
+  @Spy
+  private PatientCalculationService patientCalculationService = new PatientCalculationServiceImpl();
+
+  private StartedArtOnLastClinicalContactCalculation startedArtOnLastClinicalContactCalculation;
+
+  private TestsHelper testsHelper;
+
+  @Before
+  public void setUp(){
+    PowerMockito.mockStatic(Context.class);
+    PowerMockito.mockStatic(EptsCalculationUtils.class);
+    when(Context.getRegisteredComponents(HivMetadata.class))
+            .thenReturn(Collections.singletonList(hivMetadata));
+    when(Context.getRegisteredComponents(EPTSCalculationService.class))
+            .thenReturn(Collections.singletonList(eptsCalculationService));
+    when(Context.getService(PatientCalculationService.class)).thenReturn(patientCalculationService);
+    startedArtOnLastClinicalContactCalculation = new StartedArtOnLastClinicalContactCalculation();
+    testsHelper = new TestsHelper();
+  }
+
+  @Test
+  public void evaluateShouldReturnPatientsOnArtForLessThan90Days(){
+
+  }
+}
