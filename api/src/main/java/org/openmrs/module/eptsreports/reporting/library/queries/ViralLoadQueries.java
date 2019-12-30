e0f63f8d78f0bbd5763db0dd185544f484a21f4b
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2019-12-30 22:36:06 +0300

    Fix a bug in the viral suppresion query

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/ViralLoadQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/ViralLoadQueries.java
@@ -34,7 +34,7 @@ public class ViralLoadQueries {
             + " INNER JOIN obs o ON e.encounter_id=o.encounter_id"
             + " WHERE p.voided=0 AND e.voided=0 AND o.voided=0 AND e.encounter_type IN (%d, %d, %d, %d)"
             + " AND  o.concept_id IN(%d, %d) "
-            + " AND o.value_numeric IS NOT NULL AND"
+            + " AND (o.value_numeric < 1000 OR o.value_coded IS NOT NULL) AND"
             + " e.encounter_datetime BETWEEN date_add(date_add(:endDate, interval -12 MONTH), interval 1 day) AND :endDate AND"
             + " e.location_id=:location GROUP BY p.patient_id"
             + ") ultima_carga"
