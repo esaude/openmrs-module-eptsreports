e1a7961598ec00f351ad36da4b9400500458f795
Kelvin Murumba <kevmurumba@gmail.com>
2020-01-06 12:26:15 +0300

    Mepts 309 rm additional disaggregation tx ml (#173)
    * Added the ML excel template
    
    * Added Setup for the report
    
    * Added the report setup class
    
    * ML Report setup
    
    * Removed Not Consented, traced untraced disaggregations. Updated the SetupMERQuartelyML class.
    
    * Changed minimum days of missed appointment to 28.
    
    * Replaced the transferred out with noScheduledDrugPickupOrNextConsultation union.
    
    * Removed unused imports
    
    * Added the died composition
    
    * Changed the dead diaggregation and cleaned up the old CD
    
    * Added Transfer Out Cohort Definition
    
    * Added the Transfer Out Composition Cohort Definition.
    
    * Made changes to the Patients With Missed Visit On Mastercard query.
    
    * Added the refused/stopped treatment query. Made changes on the transfer out disaggregation
    
    * Mapping the report to excel template and added the dataset.
    
    * Added the missing concepts, 23915 and 23946.
    
    * MEPTS-300: Made changes to the Stopped sql query
    
    * Added the Untraced and Traced queries
    
    * Format
    
    * Added CD for the C1 and C2 diaggreagations.
    
    * MEPTS-300: Made changes to b and fixed the refused treatment CD
    
    * Added the LTFU shell
    
    * Added Started On ART Based on last clinical contact and added missing Concepts in HivMetadata
    
    * MEPTS-309: Added LTFU disaggregations to the Dataset
    
    * Mapped the report to excel template and added boundaries for C2.
    
    * MEPTS-309: Documentation and Registering Calculation as a Component
    
    * Changed the parameters from endDate to onOrBefore in the calculation
    
    * MEPTS-309: Made changes to the base cohort defininition, should be (a or b)
    
    * MEPTS-300: Fixed the base Cohort Definition, (a or b)
    
    * MEPST-309: Added Test shel for the StartedArtOnLastClinicalContactCalculation
    
    * MEPTS-300: Bug Fix on config.xml
    
    * MEPTS-309: Bug fix
    
    * MEPTS-309: Added date strings for the test.
    
    * MEPTS-309: Removed first if in the calculation.
    
    * MEPTS-309: Reused metadata in CommonMetadata.
    
    * MEPTS-309: Made changes to the base query, a.
    
    * MEPTS-309: Removed the if condition in calclulation.
    
    * MEPTS-309: Made changes to the unit test and Calculation
    
    * MEPTS-309: Fixed bugs and producing results. Commented out tests for now.

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/txml/StartedArtOnLastClinicalContactCalculation.java
@@ -0,0 +1,121 @@
+package org.openmrs.module.eptsreports.reporting.calculation.txml;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+import org.openmrs.calculation.patient.PatientCalculationContext;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.module.eptsreports.metadata.CommonMetadata;
+import org.openmrs.module.eptsreports.metadata.HivMetadata;
+import org.openmrs.module.eptsreports.reporting.calculation.AbstractPatientCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.BooleanResult;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.InitialArtStartDateCalculation;
+import org.openmrs.module.eptsreports.reporting.utils.EptsCalculationUtils;
+import org.openmrs.module.reporting.data.patient.definition.SqlPatientDataDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartedArtOnLastClinicalContactCalculation extends AbstractPatientCalculation {
+
+  @Override
+  public CalculationResultMap evaluate(
+      Collection<Integer> cohort,
+      Map<String, Object> parameterValues,
+      PatientCalculationContext context) {
+    HivMetadata hivMetadata = Context.getRegisteredComponents(HivMetadata.class).get(0);
+    CommonMetadata commonMetadata = Context.getRegisteredComponents(CommonMetadata.class).get(0);
+
+    CalculationResultMap lastClinicalContactMap =
+        getLastClinicalContactMap(cohort, context, commonMetadata, hivMetadata);
+
+    Boolean lessThan90Days = (Boolean) parameterValues.get("lessThan90Days");
+
+    CalculationResultMap lessThan90DaysMap = new CalculationResultMap();
+    CalculationResultMap moreThan90DaysMap = new CalculationResultMap();
+    CalculationResultMap artStartDates =
+        calculate(
+            Context.getRegisteredComponents(InitialArtStartDateCalculation.class).get(0),
+            cohort,
+            parameterValues,
+            context);
+
+    for (Integer patientId : cohort) {
+      Date artStartDate = InitialArtStartDateCalculation.getArtStartDate(patientId, artStartDates);
+      Date lastClinicalContact =
+          EptsCalculationUtils.resultForPatient(lastClinicalContactMap, patientId);
+
+      if (artStartDate != null && lastClinicalContact != null) {
+        Date onArtForLessThan90Days = EptsCalculationUtils.addDays(artStartDate, 90);
+
+        if (onArtForLessThan90Days.compareTo(lastClinicalContact) < 0) {
+          lessThan90DaysMap.put(patientId, new BooleanResult(true, this));
+        }
+        if (onArtForLessThan90Days.compareTo(lastClinicalContact) >= 0) {
+          moreThan90DaysMap.put(patientId, new BooleanResult(true, this));
+        }
+      }
+    }
+    if (lessThan90Days) {
+      return lessThan90DaysMap;
+    } else {
+      return moreThan90DaysMap;
+    }
+  }
+
+  /**
+   * @param cohort
+   * @param context
+   * @return
+   */
+  private CalculationResultMap getLastClinicalContactMap(
+      Collection<Integer> cohort,
+      PatientCalculationContext context,
+      CommonMetadata commonMetadata,
+      HivMetadata hivMetadata) {
+    SqlPatientDataDefinition sqlPatientDataDefinition = new SqlPatientDataDefinition();
+    sqlPatientDataDefinition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+    sqlPatientDataDefinition.addParameter(new Parameter("location", "location", Location.class));
+
+    String query =
+        "SELECT lcc.patient_id, MAX(lcc.value_datetime) FROM ( "
+            + "SELECT p.patient_id,MAX(o.value_datetime)value_datetime  FROM patient p "
+            + "INNER JOIN encounter e ON p.patient_id=e.patient_id "
+            + "INNER JOIN obs o ON e.encounter_id=o.encounter_id "
+            + "WHERE p.voided =0 AND e.voided=0 AND o.voided=0 AND o.concept_id=%d AND o.value_datetime IS NOT NULL AND e.encounter_type =%d AND e.location_id= :location "
+            + "AND e.encounter_datetime <= :onOrBefore GROUP BY p.patient_id "
+            + "UNION "
+            + "SELECT p.patient_id,MAX(o.value_datetime)value_datetime  FROM patient p "
+            + "INNER JOIN encounter e ON p.patient_id=e.patient_id "
+            + "INNER JOIN obs o ON e.encounter_id=o.encounter_id "
+            + "WHERE p.voided =0 AND e.voided=0 AND o.voided=0 AND o.concept_id=%d AND o.value_datetime IS NOT NULL AND e.encounter_type =%d AND e.location_id= :location "
+            + "AND e.encounter_datetime <= :onOrBefore GROUP BY p.patient_id "
+            + "UNION "
+            + "SELECT p.patient_id,DATE_ADD(MAX(o.value_datetime), INTERVAL 30 day) value_datetime  FROM patient p "
+            + "INNER JOIN encounter e ON p.patient_id=e.patient_id "
+            + "INNER JOIN obs o ON e.encounter_id=o.encounter_id "
+            + "WHERE p.voided =0 AND e.voided=0 AND o.voided=0 AND o.concept_id=%d AND o.value_datetime IS NOT NULL AND e.encounter_type =%d AND e.location_id= :location "
+            + "AND e.encounter_datetime <= :onOrBefore GROUP BY p.patient_id "
+            + ")lcc GROUP BY patient_id ";
+
+    sqlPatientDataDefinition.setSql(
+        String.format(
+            query,
+            commonMetadata.getReturnVisitDateConcept().getConceptId(),
+            hivMetadata.getARVPharmaciaEncounterType().getEncounterTypeId(),
+            commonMetadata.getReturnVisitDateConcept().getConceptId(),
+            hivMetadata.getARVPharmaciaEncounterType().getEncounterTypeId(),
+            hivMetadata.getArtDatePickup().getConceptId(),
+            hivMetadata.getMasterCardDrugPickupEncounterType().getEncounterTypeId()));
+
+    Map<String, Object> params = new HashMap<>();
+    params.put("onOrBefore", context.getFromCache("onOrBefore"));
+    params.put("location", context.getFromCache("location"));
+
+    return EptsCalculationUtils.evaluateWithReporting(
+        sqlPatientDataDefinition, cohort, params, null, context);
+  }
+}
