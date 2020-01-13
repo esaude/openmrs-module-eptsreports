8d0e80333d22db2bd8a6c67af6c4c8a202602d76
ningosi <nicotwendelee@gmail.com>
2020-01-13 21:51:40 +0300

    Add the query change required for the B1 indicator

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/GenericCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/GenericCohortQueries.java
@@ -470,18 +470,19 @@ public class GenericCohortQueries {
 
   /**
    * Get patients who have encounter of a specific type within date boundaries
+   *
    * @param encounterType
    * @retrun CohortDefinition
    */
   public CohortDefinition getPatientsHavingEncounterWithinDateBoundaries(int encounterType) {
-    String query = "";
+    String query =
+        "SELECT p.patient_id FROM patient p INNER JOIN encounter e ON p.patient_id=e.patient_id WHERE e.voided=0 AND p.voided=0 AND e.encounter_type = %d AND e.encounter_datetime BETWEEN :startDate AND :endDate";
     SqlCohortDefinition cd = new SqlCohortDefinition();
-    cd.setName("Patients having encounter type "+ encounterType);
+    cd.setName("Patients having encounter type " + encounterType);
     cd.addParameter(new Parameter("location", "Location", Location.class));
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("EndDate", "End Date", Date.class));
-    cd.setQuery(query);
+    cd.setQuery(String.format(query, encounterType));
     return cd;
-
   }
 }
