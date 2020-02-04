6d2a0fafd23dd63e43817ec549b9a3e3cfb7a0a6
guimino <minoneves@gmail.com>
2020-02-04 14:34:17 +0200

    refactored txml report indicator

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/txml/TxMLAbstractPatientCalculation.java
@@ -0,0 +1,131 @@
+package org.openmrs.module.eptsreports.reporting.calculation.txml;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+import org.openmrs.api.context.Context;
+import org.openmrs.calculation.result.CalculationResult;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.calculation.result.SimpleResult;
+import org.openmrs.module.eptsreports.reporting.calculation.BaseFghCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.BooleanResult;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.LastFilaCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.LastRecepcaoLevantamentoCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.LastSeguimentoCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.NextFilaDateCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.NextSeguimentoDateCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.generic.OnArtInitiatedArvDrugsCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.util.processor.CalculationProcessorUtils;
+import org.openmrs.module.reporting.common.DateUtil;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+
+public abstract class TxMLAbstractPatientCalculation extends BaseFghCalculation {
+
+  public static int DAYS_TO_LTFU = 28;
+
+  @Override
+  public CalculationResultMap evaluate(
+      Map<String, Object> parameterValues, EvaluationContext context) {
+    CalculationResultMap resultMap = new CalculationResultMap();
+
+    Date startDate = (Date) context.getParameterValues().get("startDate");
+    Date endDate = (Date) context.getParameterValues().get("endDate");
+
+    CalculationResultMap inicioRealResult =
+        Context.getRegisteredComponents(OnArtInitiatedArvDrugsCalculation.class)
+            .get(0)
+            .evaluate(parameterValues, context);
+    Set<Integer> cohort = inicioRealResult.keySet();
+    CalculationResultMap lastFilaCalculationResult =
+        Context.getRegisteredComponents(LastFilaCalculation.class)
+            .get(0)
+            .evaluate(cohort, parameterValues, context);
+    CalculationResultMap lastSeguimentoCalculationResult =
+        Context.getRegisteredComponents(LastSeguimentoCalculation.class)
+            .get(0)
+            .evaluate(cohort, parameterValues, context);
+    LastRecepcaoLevantamentoCalculation lastRecepcaoLevantamentoCalculation =
+        Context.getRegisteredComponents(LastRecepcaoLevantamentoCalculation.class).get(0);
+    CalculationResultMap lastRecepcaoLevantamentoResult =
+        lastRecepcaoLevantamentoCalculation.evaluate(cohort, parameterValues, context);
+
+    CalculationResultMap nextFilaResult =
+        Context.getRegisteredComponents(NextFilaDateCalculation.class)
+            .get(0)
+            .evaluate(lastFilaCalculationResult.keySet(), parameterValues, context);
+    CalculationResultMap nextSeguimentoResult =
+        Context.getRegisteredComponents(NextSeguimentoDateCalculation.class)
+            .get(0)
+            .evaluate(lastSeguimentoCalculationResult.keySet(), parameterValues, context);
+
+    return this.evaluateUsingCalculationRules(
+        cohort,
+        startDate,
+        endDate,
+        resultMap,
+        inicioRealResult,
+        lastFilaCalculationResult,
+        lastSeguimentoCalculationResult,
+        nextFilaResult,
+        nextSeguimentoResult,
+        lastRecepcaoLevantamentoResult,
+        lastRecepcaoLevantamentoCalculation);
+  }
+
+  @Override
+  public CalculationResultMap evaluate(
+      Collection<Integer> cohort, Map<String, Object> parameterValues, EvaluationContext context) {
+    return this.evaluate(parameterValues, context);
+  }
+
+  protected abstract CalculationResultMap evaluateUsingCalculationRules(
+      Set<Integer> cohort,
+      Date startDate,
+      Date endDate,
+      CalculationResultMap resultMap,
+      CalculationResultMap inicioRealResult,
+      CalculationResultMap lastFilaCalculationResult,
+      CalculationResultMap lastSeguimentoCalculationResult,
+      CalculationResultMap nextFilaResult,
+      CalculationResultMap nextSeguimentoResult,
+      CalculationResultMap lastRecepcaoLevantamentoResult,
+      LastRecepcaoLevantamentoCalculation lastRecepcaoLevantamentoCalculation);
+
+  protected void checkConsultationsOrFilaWithoutNextConsultationDate(
+      Integer patientId,
+      CalculationResultMap resultMap,
+      Date endDate,
+      CalculationResultMap lastResult,
+      CalculationResultMap nextResult) {
+
+    CalculationResult calculationLastResult = lastResult.get(patientId);
+    CalculationResult calculationNextResult = nextResult.get(patientId);
+
+    if (calculationNextResult != null && calculationNextResult.getValue() == null) {
+      if (calculationLastResult != null) {
+        Date lastDate = (Date) calculationLastResult.getValue();
+        if (DateUtil.getDaysBetween(lastDate, endDate) >= 0) {
+          resultMap.put(patientId, new BooleanResult(Boolean.TRUE, this));
+        }
+      }
+    }
+  }
+
+  public static CalculationResultMap getLastRecepcaoLevantamentoPlus30(
+      Integer patientId,
+      CalculationResultMap lastRecepcaoLevantamentoResult,
+      LastRecepcaoLevantamentoCalculation lastRecepcaoLevantamentoCalculation) {
+
+    CalculationResultMap lastRecepcaoLevantamentoPlus30 = new CalculationResultMap();
+    CalculationResult maxRecepcao = lastRecepcaoLevantamentoResult.get(patientId);
+    if (maxRecepcao != null) {
+      lastRecepcaoLevantamentoPlus30.put(
+          patientId,
+          new SimpleResult(
+              CalculationProcessorUtils.adjustDaysInDate((Date) maxRecepcao.getValue(), 30),
+              lastRecepcaoLevantamentoCalculation));
+    }
+    return lastRecepcaoLevantamentoPlus30;
+  }
+}
--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/txml/TxMLPatientsWhoMissedNextApointmentCalculation.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/txml/TxMLPatientsWhoMissedNextApointmentCalculation.java
@@ -1,68 +1,32 @@
 package org.openmrs.module.eptsreports.reporting.calculation.txml;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.Map;
 import java.util.Set;
-import org.openmrs.api.context.Context;
 import org.openmrs.calculation.result.CalculationResult;
 import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.calculation.result.SimpleResult;
-import org.openmrs.module.eptsreports.reporting.calculation.BaseFghCalculation;
-import org.openmrs.module.eptsreports.reporting.calculation.generic.LastFilaCalculation;
 import org.openmrs.module.eptsreports.reporting.calculation.generic.LastRecepcaoLevantamentoCalculation;
-import org.openmrs.module.eptsreports.reporting.calculation.generic.LastSeguimentoCalculation;
-import org.openmrs.module.eptsreports.reporting.calculation.generic.NextFilaDateCalculation;
-import org.openmrs.module.eptsreports.reporting.calculation.generic.NextSeguimentoDateCalculation;
-import org.openmrs.module.eptsreports.reporting.calculation.generic.OnArtInitiatedArvDrugsCalculation;
 import org.openmrs.module.eptsreports.reporting.calculation.util.processor.CalculationProcessorUtils;
 import org.openmrs.module.reporting.common.DateUtil;
-import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TxMLPatientsWhoMissedNextApointmentCalculation extends BaseFghCalculation {
+public class TxMLPatientsWhoMissedNextApointmentCalculation extends TxMLAbstractPatientCalculation {
 
   public static int DAYS_TO_LTFU = 28;
 
-  @Override
-  public CalculationResultMap evaluate(
-      Map<String, Object> parameterValues, EvaluationContext context) {
-    CalculationResultMap resultMap = new CalculationResultMap();
-
-    Date startDate = (Date) context.getParameterValues().get("startDate");
-    Date endDate = (Date) context.getParameterValues().get("endDate");
-
-    CalculationResultMap inicioRealResult =
-        Context.getRegisteredComponents(OnArtInitiatedArvDrugsCalculation.class)
-            .get(0)
-            .evaluate(parameterValues, context);
-
-    Set<Integer> cohort = inicioRealResult.keySet();
-
-    LastRecepcaoLevantamentoCalculation lastRecepcaoLevantamentoCalculation =
-        Context.getRegisteredComponents(LastRecepcaoLevantamentoCalculation.class).get(0);
-    CalculationResultMap lastRecepcaoLevantamentoResult =
-        lastRecepcaoLevantamentoCalculation.evaluate(cohort, parameterValues, context);
-
-    CalculationResultMap lastFilaCalculationResult =
-        Context.getRegisteredComponents(LastFilaCalculation.class)
-            .get(0)
-            .evaluate(cohort, parameterValues, context);
-    CalculationResultMap lastSeguimentoCalculation =
-        Context.getRegisteredComponents(LastSeguimentoCalculation.class)
-            .get(0)
-            .evaluate(cohort, parameterValues, context);
-
-    CalculationResultMap nextFilaResult =
-        Context.getRegisteredComponents(NextFilaDateCalculation.class)
-            .get(0)
-            .evaluate(lastFilaCalculationResult.keySet(), parameterValues, context);
-
-    CalculationResultMap nextSeguimentoResult =
-        Context.getRegisteredComponents(NextSeguimentoDateCalculation.class)
-            .get(0)
-            .evaluate(lastSeguimentoCalculation.keySet(), parameterValues, context);
+  protected CalculationResultMap evaluateUsingCalculationRules(
+      Set<Integer> cohort,
+      Date startDate,
+      Date endDate,
+      CalculationResultMap resultMap,
+      CalculationResultMap inicioRealResult,
+      CalculationResultMap lastFilaCalculationResult,
+      CalculationResultMap lastSeguimentoCalculationResult,
+      CalculationResultMap nextFilaResult,
+      CalculationResultMap nextSeguimentoResult,
+      CalculationResultMap lastRecepcaoLevantamentoResult,
+      LastRecepcaoLevantamentoCalculation lastRecepcaoLevantamentoCalculation) {
 
     for (Integer patientId : cohort) {
 
@@ -83,19 +47,14 @@ public class TxMLPatientsWhoMissedNextApointmentCalculation extends BaseFghCalcu
         this.checkConsultationsOrFilaWithoutNextConsultationDate(
             patientId, resultMap, endDate, lastFilaCalculationResult, nextFilaResult);
         this.checkConsultationsOrFilaWithoutNextConsultationDate(
-            patientId, resultMap, endDate, lastSeguimentoCalculation, nextSeguimentoResult);
+            patientId, resultMap, endDate, lastSeguimentoCalculationResult, nextSeguimentoResult);
       }
     }
     return resultMap;
   }
 
   @Override
-  public CalculationResultMap evaluate(
-      Collection<Integer> cohort, Map<String, Object> parameterValues, EvaluationContext context) {
-    return this.evaluate(parameterValues, context);
-  }
-
-  private void checkConsultationsOrFilaWithoutNextConsultationDate(
+  protected void checkConsultationsOrFilaWithoutNextConsultationDate(
       Integer patientId,
       CalculationResultMap resultMap,
       Date endDate,
@@ -110,26 +69,8 @@ public class TxMLPatientsWhoMissedNextApointmentCalculation extends BaseFghCalcu
         Date lastDate = (Date) calculationLastResult.getValue();
         if (DateUtil.getDaysBetween(lastDate, endDate) >= 0) {
           resultMap.put(patientId, new SimpleResult(lastDate, this));
-          System.out.println("Patient without consultation " + patientId);
         }
       }
     }
-  }
-
-  public static CalculationResultMap getLastRecepcaoLevantamentoPlus30(
-      Integer patientId,
-      CalculationResultMap lastRecepcaoLevantamentoResult,
-      LastRecepcaoLevantamentoCalculation lastRecepcaoLevantamentoCalculation) {
-
-    CalculationResultMap lastRecepcaoLevantamentoPlus30 = new CalculationResultMap();
-    CalculationResult maxRecepcao = lastRecepcaoLevantamentoResult.get(patientId);
-    if (maxRecepcao != null) {
-      lastRecepcaoLevantamentoPlus30.put(
-          patientId,
-          new SimpleResult(
-              CalculationProcessorUtils.adjustDaysInDate((Date) maxRecepcao.getValue(), 30),
-              lastRecepcaoLevantamentoCalculation));
-    }
-    return lastRecepcaoLevantamentoPlus30;
   }
 }
