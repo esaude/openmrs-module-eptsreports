7d90dd954cfdf7dd51f493e5037abf41a8ccca59
Yassin <yassin.nurmahomed@jembi.org>
2020-02-24 11:35:22 +0200

    Resumo mensal transferred in cohort (#214)
    * Create re-usable transferred-in cohort
    
    * Extract parameter answer for TYPE OF PATIENT TRANSFERRED FROM

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/cohort/definition/EptsTransferredInCohortDefinition.java
@@ -0,0 +1,58 @@
+package org.openmrs.module.eptsreports.reporting.cohort.definition;
+
+import java.util.Date;
+import org.openmrs.Concept;
+import org.openmrs.Location;
+import org.openmrs.module.reporting.cohort.definition.BaseCohortDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyAndParameterCachingStrategy;
+import org.openmrs.module.reporting.evaluation.caching.Caching;
+
+/**
+ * Cohort for transferred-in patients as specified in Resumo Mensal. Should e used by other reports
+ * in the future.
+ */
+@Caching(strategy = ConfigurationPropertyAndParameterCachingStrategy.class)
+public class EptsTransferredInCohortDefinition extends BaseCohortDefinition {
+
+  @ConfigurationProperty private Date onOrAfter;
+
+  @ConfigurationProperty private Date onOrBefore;
+
+  @ConfigurationProperty private Location location;
+
+  @ConfigurationProperty(required = true)
+  private Concept typeOfPatientTransferredFromAnswer;
+
+  public Date getOnOrBefore() {
+    return onOrBefore;
+  }
+
+  public void setOnOrBefore(Date onOrBefore) {
+    this.onOrBefore = onOrBefore;
+  }
+
+  public Date getOnOrAfter() {
+    return onOrAfter;
+  }
+
+  public void setOnOrAfter(Date onOrAfter) {
+    this.onOrAfter = onOrAfter;
+  }
+
+  public Location getLocation() {
+    return location;
+  }
+
+  public void setLocation(Location location) {
+    this.location = location;
+  }
+
+  public Concept getTypeOfPatientTransferredFromAnswer() {
+    return typeOfPatientTransferredFromAnswer;
+  }
+
+  public void setTypeOfPatientTransferredFromAnswer(Concept typeOfPatientTransferredFromAnswer) {
+    this.typeOfPatientTransferredFromAnswer = typeOfPatientTransferredFromAnswer;
+  }
+}
