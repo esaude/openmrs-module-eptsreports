33a37fedcc132c9a1ebbbf0999be4242f49d14b4
amosh101 <amosh101@outlook.com>
2020-02-25 18:24:58 +0200

    TESTS

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/ResumoMensalCohortQueriesTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/ResumoMensalCohortQueriesTest.java
@@ -1,24 +1,35 @@
 package org.openmrs.module.eptsreports.reporting.intergrated.library.cohorts;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Before;
+import org.junit.Test;
 import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.eptsreports.reporting.intergrated.utils.DefinitionsTest;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.ResumoMensalCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.TxCurrCohortQueries;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.common.DateUtil;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
-
 public class ResumoMensalCohortQueriesTest extends DefinitionsTest {
 
-  @Autowired private ResumoMensalCohortQueries resumoMensalCohortQueries;
+    @Autowired private ResumoMensalCohortQueries resumoMensalCohortQueries;
 
   @Before
   public void setup() throws Exception {
     executeDataSet("ResumoMensalTest.xml");
   }
+
 
   @Override
   protected Date getStartDate() {
@@ -42,5 +53,28 @@ public class ResumoMensalCohortQueriesTest extends DefinitionsTest {
     context.addParameterValue("startDate", startDate);
     context.addParameterValue("onOrBefore", endDate);
     context.addParameterValue("location", location);
+  }
+
+  @Test
+  public void getDeadPatientsShouldReturn()
+      throws EvaluationException {
+
+    CohortDefinition cohort =
+    resumoMensalCohortQueries.getPatientsWhoDied(true);
+    EvaluatedCohort evaluatedCohort = evaluateCohortDefinition(cohort);
+
+    assertEquals(5, evaluatedCohort.getMemberIds().size());
+
+    // DEAD DEMOGRAPHIC
+    assertTrue(evaluatedCohort.getMemberIds().contains(1020));
+
+    // DEAD DEMOGRAPHIC WITH ENCOUNTER
+    assertFalse(evaluatedCohort.getMemberIds().contains(1021));
+
+    // DEAD ART
+    assertTrue(evaluatedCohort.getMemberIds().contains(1022));
+
+    // DEAD PRE ART
+    assertTrue(evaluatedCohort.getMemberIds().contains(1023));
   }
 }
