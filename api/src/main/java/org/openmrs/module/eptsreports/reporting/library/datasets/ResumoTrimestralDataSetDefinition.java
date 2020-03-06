d85c492ed268949b98c966d472046ea6d16a3fbe
Yassin Nurmahomed <yassin.nurmahomed@jembi.org>
2020-03-06 14:51:57 +0200

    Use year and quarter as parameters

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/ResumoTrimestralDataSetDefinition.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/ResumoTrimestralDataSetDefinition.java
@@ -1,7 +1,6 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
@@ -11,15 +10,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class ResumoTrimestralDataSetDefinition extends BaseDataSet {
 
+  enum Quarter {
+    Q1("1st"),
+    Q2("2nd"),
+    Q3("3rd");
+
+    private String display;
+
+    Quarter(String display) {
+      this.display = display;
+    }
+
+    @Override
+    public String toString() {
+      return display;
+    }
+  };
+
   public DataSetDefinition constructResumoTrimestralDataset() {
-    return new CohortIndicatorDataSetDefinition();
+    CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+    dsd.setName("Resumo trimestral data set");
+    return dsd;
   }
 
   @Override
   public List<Parameter> getParameters() {
     ArrayList<Parameter> parameters = new ArrayList<>();
-    parameters.add(new Parameter("quarter", "Quarter", Integer.class));
-    parameters.add(new Parameter("endDate", "End date", Date.class));
+    parameters.add(new Parameter("year", "Year", Integer.class));
+    parameters.add(new Parameter("quarter", "Quarter", Quarter.class));
     return parameters;
   }
 }
