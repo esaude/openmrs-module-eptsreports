37c89edf9c9236abbf828893de209dcc410275ad
Yassin Nurmahomed <yassin.nurmahomed@jembi.org>
2020-03-15 14:55:36 +0200

    MEPTS-341 Implement indicator A

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/cohort/definition/EptsQuarterlyCohortDefinition.java
@@ -0,0 +1,80 @@
+package org.openmrs.module.eptsreports.reporting.cohort.definition;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.openmrs.Location;
+import org.openmrs.module.reporting.cohort.definition.BaseCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyAndParameterCachingStrategy;
+import org.openmrs.module.reporting.evaluation.caching.Caching;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+
+/**
+ * A cohort definition that computes date ranges for a specific month of a quarter of a year and
+ * evaluates another wrapped cohort definition with the computed dates as parameter values.
+ */
+@Caching(strategy = ConfigurationPropertyAndParameterCachingStrategy.class)
+public class EptsQuarterlyCohortDefinition extends BaseCohortDefinition {
+
+  public enum Month {
+    M1,
+    M2,
+    M3
+  };
+
+  @ConfigurationProperty private Month month;
+
+  @ConfigurationProperty private Integer year;
+
+  @ConfigurationProperty private Location location;
+
+  @ConfigurationProperty private CohortDefinition cohortDefinition;
+
+  public EptsQuarterlyCohortDefinition() {}
+
+  public EptsQuarterlyCohortDefinition(CohortDefinition cohortDefinition, Month month) {
+    setCohortDefinition(cohortDefinition);
+    this.month = month;
+  }
+
+  public Month getMonth() {
+    return month;
+  }
+
+  public void setMonth(Month month) {
+    this.month = month;
+  }
+
+  public CohortDefinition getCohortDefinition() {
+    return cohortDefinition;
+  }
+
+  public void setCohortDefinition(CohortDefinition cohortDefinition) {
+    List<Parameter> parameters = new ArrayList<>();
+    parameters.add(new Parameter("onOrBefore", "Before Date", Date.class));
+    parameters.add(new Parameter("onOrAfter", "After Date", Date.class));
+    if (!cohortDefinition.getParameters().containsAll(parameters)) {
+      throw new IllegalArgumentException(
+          "CohortDefinition must have onOrAfter and onOrBefore parameters");
+    }
+    this.cohortDefinition = cohortDefinition;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  public Location getLocation() {
+    return location;
+  }
+
+  public void setLocation(Location location) {
+    this.location = location;
+  }
+}
--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/ResumoTrimestralDataSetDefinition.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/ResumoTrimestralDataSetDefinition.java
@@ -3,23 +3,24 @@ package org.openmrs.module.eptsreports.reporting.library.datasets;
 import static org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils.map;
 import static org.openmrs.module.reporting.evaluation.parameter.Mapped.mapStraightThrough;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import org.openmrs.Location;
+import org.openmrs.module.eptsreports.reporting.cohort.definition.EptsQuarterlyCohortDefinition;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
-import org.openmrs.module.reporting.ReportingConstants;
-import org.openmrs.module.reporting.cohort.definition.AllPatientsCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.indicator.CohortIndicator;
-import org.openmrs.module.reporting.indicator.dimension.CohortDefinitionDimension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ResumoTrimestralDataSetDefinition extends BaseDataSet {
+
+  public static final String NO_DIMENSION_OPTIONS = "";
 
   public static final String A =
       "Nº de pacientes que iniciou TARV nesta unidade sanitária durante o mês";
@@ -39,42 +40,25 @@ public class ResumoTrimestralDataSetDefinition extends BaseDataSet {
     CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
     dsd.setName("Resumo trimestral data set");
     dsd.addParameters(getParameters());
-    dsd.addDimension("month", mapStraightThrough(getADimension()));
-    addRow(dsd, "A", "A", getAllPatientsIndicator(), getColumns());
+    dsd.addColumn("Am1", A, getA(EptsQuarterlyCohortDefinition.Month.M1), NO_DIMENSION_OPTIONS);
+    dsd.addColumn("Am2", A, getA(EptsQuarterlyCohortDefinition.Month.M2), NO_DIMENSION_OPTIONS);
+    dsd.addColumn("Am3", A, getA(EptsQuarterlyCohortDefinition.Month.M3), NO_DIMENSION_OPTIONS);
     return dsd;
   }
 
-  /**
-   * @return Return an indicator of all patients. Make sure the report definition uses a base
-   *     cohort.
-   */
-  private Mapped<CohortIndicator> getAllPatientsIndicator() {
-    CohortDefinition cd = new AllPatientsCohortDefinition();
-    CohortIndicator indicator =
-        eptsGeneralIndicator.getIndicator(A, mapStraightThrough(cd), getParameters());
-    return mapStraightThrough(indicator);
+  private Mapped<CohortIndicator> getA(EptsQuarterlyCohortDefinition.Month month) {
+    CohortDefinition wrap = genericCohortQueries.getStartedArtOnPeriod(false, true);
+    CohortDefinition quarterly = getQuarterlyCohort(wrap, month);
+    String mappings = "onOrAfter=${startDate},onOrBefore=${endDate},location=${location}";
+    return mapStraightThrough(eptsGeneralIndicator.getIndicator(A, map(quarterly, mappings)));
   }
 
-  private CohortDefinitionDimension getADimension() {
-    CohortDefinitionDimension dim = new CohortDefinitionDimension();
-    dim.addParameter(ReportingConstants.START_DATE_PARAMETER);
-    dim.addParameter(ReportingConstants.END_DATE_PARAMETER);
-    dim.addParameter(ReportingConstants.LOCATION_PARAMETER);
-    CohortDefinition cd = genericCohortQueries.getStartedArtOnPeriod(false, true);
-    dim.addCohortDefinition(
-        "m1", map(cd, "onOrAfter=${startDate},onOrBefore=${endDate-2m},location=${location}"));
-    dim.addCohortDefinition(
-        "m2", map(cd, "onOrAfter=${startDate+1m},onOrBefore=${endDate-1m},location=${location}"));
-    dim.addCohortDefinition(
-        "m3", map(cd, "onOrAfter=${startDate+2m},onOrBefore=${endDate},location=${location}"));
-    return dim;
-  }
-
-  private List<ColumnParameters> getColumns() {
-    ArrayList<ColumnParameters> cols = new ArrayList<>();
-    cols.add(new ColumnParameters("m1", "First month of quarter", "month=m1", "01"));
-    cols.add(new ColumnParameters("m2", "Second month of quarter", "month=m2", "02"));
-    cols.add(new ColumnParameters("m3", "Third month of quarter", "month=m3", "03"));
-    return cols;
+  private EptsQuarterlyCohortDefinition getQuarterlyCohort(
+      CohortDefinition wrap, EptsQuarterlyCohortDefinition.Month month) {
+    EptsQuarterlyCohortDefinition cd = new EptsQuarterlyCohortDefinition(wrap, month);
+    cd.addParameter(new Parameter("onOrAfter", "onOrAfter", Date.class));
+    cd.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+    cd.addParameter(new Parameter("location", "location", Location.class));
+    return cd;
   }
 }
