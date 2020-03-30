adf4f250cc07bf7b584c988f7cb4d263eff32fe8
ningosi <nicotwendelee@gmail.com>
2020-03-30 19:06:57 +0300

    Add the endDate parameter in the base cohort for the trimester report

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupResumoTrimestralReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupResumoTrimestralReport.java
@@ -77,7 +77,7 @@ public class SetupResumoTrimestralReport extends EptsDataExportManager {
         "R",
         mapStraightThrough(resumoTrimestralDataSetDefinition.constructResumoTrimestralDataset()));
     rd.setBaseCohortDefinition(
-        EptsReportUtils.map(genericCohortQueries.getBaseCohort(), "location=${location}"));
+        EptsReportUtils.map(genericCohortQueries.getBaseCohort(), "location=${location},endDate=${endDate}"));
     return rd;
   }
 
