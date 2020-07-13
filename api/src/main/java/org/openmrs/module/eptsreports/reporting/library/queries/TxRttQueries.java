5e43732123c5ce198da11eee5ebdb3c4a6a7e2d3
Paulo Sérgio Jaime Matsinhe <psmatsinhe@gmail.com>
2020-07-13 12:02:56 +0200

    MEPTS-558: fix rtt query (#161)
    * MEPTS-558: fix rtt query
    
    * MEPTS-588: removing comma
    
    * MEPTS-558: fix case of various encounter in the same day
    
    * MEPTS-558: fix most recent pickup of the recent encounter date

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TxRttQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TxRttQueries.java
@@ -13,6 +13,10 @@
  */
 package org.openmrs.module.eptsreports.reporting.library.queries;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.text.StringSubstitutor;
+
 public class TxRttQueries {
 
   /**
@@ -26,7 +30,7 @@ public class TxRttQueries {
    * @param masterCardEncounter
    * @param consultationConcept
    * @param nextDrugPickupConcept
-   * @param dateOfArtPickupConcept
+   * @param dateOfArtPickupConceptMasterCard
    * @return String
    */
   public static String getAllPatientsWhoMissedPreviousAppointmentBy28Days(
@@ -36,76 +40,162 @@ public class TxRttQueries {
       int masterCardEncounter,
       int consultationConcept,
       int nextDrugPickupConcept,
-      int dateOfArtPickupConcept) {
+      int dateOfArtPickupConceptMasterCard) {
     String query =
-        "SELECT e.patient_id FROM "
-            + " (SELECT"
-            + "     e.encounter_id, "
-            + "     p.patient_id, "
-            + "     ("
-            + "       SELECT oo.value_datetime "
-            + "       FROM encounter ee "
-            + "         INNER JOIN obs oo ON "
-            + "           ee.encounter_id = oo.encounter_id "
-            + "       WHERE "
-            + "         ee.voided = 0 "
-            + "         AND oo.voided = 0 "
-            + "         AND ee.encounter_type IN (%d,%d) "
-            + "         AND oo.concept_id = %d "
-            + "         AND ee.patient_id = e.patient_id "
-            + "         AND ee.encounter_datetime < e.encounter_datetime "
-            + "         AND ee.location_id = :location "
-            + "         AND ee.encounter_datetime <= :endDate "
-            + "       ORDER BY ee.encounter_datetime DESC "
-            + "       LIMIT 1) AS prev_consultation_scheduled_date, "
-            + "     (SELECT "
-            + "       CASE WHEN ee.encounter_type = %d THEN oo.value_datetime "
-            + "       ELSE DATE_ADD(oo.value_datetime,"
-            + "       INTERVAL 30 DAY) END AS next_schedule_date "
-            + "     FROM encounter ee "
-            + "       INNER JOIN obs oo ON "
-            + "         ee.encounter_id = oo.encounter_id "
-            + "     WHERE "
-            + "       ee.voided = 0 "
-            + "       AND oo.voided = 0 "
-            + "       AND ( "
-            + "         (ee.encounter_type = %d	AND oo.concept_id = %d) "
-            + "         OR (ee.encounter_type = %d AND oo.concept_id = %d) "
-            + "       )"
-            + "       AND ee.patient_id = e.patient_id "
-            + "       AND ee.encounter_datetime < e.encounter_datetime "
-            + "       AND ee.location_id = :location "
-            + "       AND ee.encounter_datetime <= :endDate "
-            + "     ORDER BY "
-            + "       ee.encounter_datetime DESC "
-            + "     LIMIT 1 ) AS prev_drug_pickup_scheduled_date, "
-            + "     e.encounter_datetime "
-            + "   FROM patient p "
-            + "     INNER JOIN encounter e ON "
-            + "       e.patient_id = p.patient_id "
-            + "   WHERE "
-            + "     p.voided = 0 "
-            + "     AND e.voided = 0 "
-            + "     AND e.encounter_type IN (%d,%d,%d,%d) "
-            + "     AND e.location_id = :location ) e "
-            + " WHERE "
-            + "   TIMESTAMPDIFF(DAY,e.prev_consultation_scheduled_date,e.encounter_datetime) > 28 AND "
-            + "   TIMESTAMPDIFF(DAY,e.prev_drug_pickup_scheduled_date,e.encounter_datetime) > 28 AND "
-            + "   e.encounter_datetime BETWEEN :startDate AND :endDate "
-            + " GROUP BY patient_id";
-    return String.format(
-        query,
-        adultoSegEncounter,
-        pediatriaSegEncounter,
-        consultationConcept,
-        pharmacyEncounter,
-        pharmacyEncounter,
-        nextDrugPickupConcept,
-        masterCardEncounter,
-        dateOfArtPickupConcept,
-        adultoSegEncounter,
-        pediatriaSegEncounter,
-        pharmacyEncounter,
-        masterCardEncounter);
+        "SELECT  "
+            + "    patient_id "
+            + " FROM "
+            + "    (SELECT  "
+            + "        e.encounter_id, "
+            + "            p.patient_id, "
+            + "            (SELECT  oo.value_datetime "
+            + "                FROM "
+            + "                    encounter ee "
+            + "                INNER JOIN obs oo ON ee.encounter_id = oo.encounter_id "
+            + "                WHERE "
+            + "                    IF(e.encounter_type = ${masterCardEncounter}, o.value_datetime, e.encounter_datetime) > ee.encounter_datetime "
+            + "                        AND ee.voided = 0 "
+            + "                        AND oo.voided = 0 "
+            + "                        AND ee.encounter_type IN (${adultoSegEncounter} , ${pediatriaSegEncounter}) "
+            + "                        AND oo.concept_id = ${consultationConcept} "
+            + "                        AND ee.patient_id = e.patient_id "
+            + "                        AND ee.location_id = :location "
+            + "                        AND ee.encounter_datetime <= :endDate "
+            + "                ORDER BY DATE(ee.encounter_datetime) DESC, DATE(oo.value_datetime) DESC"
+            + "                LIMIT 1) AS prev_consultation_scheduled_date, "
+            + "            (SELECT  oo.value_datetime "
+            + "                FROM "
+            + "                    encounter ee "
+            + "                INNER JOIN obs oo ON ee.encounter_id = oo.encounter_id "
+            + "                WHERE "
+            + "                    IF(e.encounter_type = ${masterCardEncounter}, o.value_datetime, e.encounter_datetime) > ee.encounter_datetime "
+            + "                        AND ee.voided = 0 "
+            + "                        AND oo.voided = 0 "
+            + "                        AND ee.encounter_type = ${pharmacyEncounter} "
+            + "                        AND oo.concept_id = ${nextDrugPickupConcept} "
+            + "                        AND ee.patient_id = e.patient_id "
+            + "                        AND ee.location_id = :location "
+            + "                        AND ee.encounter_datetime <= :endDate "
+            + "                ORDER BY DATE(ee.encounter_datetime) DESC, DATE(oo.value_datetime) DESC"
+            + "                LIMIT 1) AS prev_drug_pickup_scheduled_date_fila, "
+            + "            (SELECT  "
+            + "                    DATE_ADD(oo.value_datetime, INTERVAL 30 DAY) AS value_datetime "
+            + "                FROM "
+            + "                    encounter ee "
+            + "                INNER JOIN obs oo ON ee.encounter_id = oo.encounter_id "
+            + "                WHERE "
+            + "                    IF(e.encounter_type =  ${masterCardEncounter}, o.value_datetime, e.encounter_datetime) > oo.value_datetime "
+            + "                        AND ee.voided = 0 "
+            + "                        AND oo.voided = 0 "
+            + "                        AND ee.encounter_type =  ${masterCardEncounter} "
+            + "                        AND oo.concept_id = ${dateOfArtPickupConceptMasterCard} "
+            + "                        AND ee.patient_id = e.patient_id "
+            + "                        AND ee.location_id = :location "
+            + "                        AND oo.value_datetime <= :endDate "
+            + "                ORDER BY oo.value_datetime DESC "
+            + "                LIMIT 1) AS prev_drug_pickup_scheduled_date_master_card, "
+            + "            e.encounter_datetime, "
+            + "            e.encounter_type, "
+            + "            o.value_datetime "
+            + "    FROM "
+            + "        patient p "
+            + "    INNER JOIN encounter e ON e.patient_id = p.patient_id "
+            + "    JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "    WHERE "
+            + "        p.voided = 0 AND e.voided = 0 "
+            + "            AND e.encounter_type IN (${adultoSegEncounter} , ${pediatriaSegEncounter}, ${pharmacyEncounter},  ${masterCardEncounter}) "
+            + "            AND e.location_id = :location) e "
+            + "WHERE "
+            + "    (e.encounter_type =  ${masterCardEncounter} "
+            + "        AND e.value_datetime BETWEEN :startDate AND :endDate "
+            + "        AND TIMESTAMPDIFF(DAY, "
+            + "        e.prev_consultation_scheduled_date, "
+            + "        e.value_datetime) > 28 "
+            + "        AND (CASE "
+            + "        WHEN "
+            + "            prev_drug_pickup_scheduled_date_fila IS NOT NULL "
+            + "                AND prev_drug_pickup_scheduled_date_master_card IS NULL "
+            + "        THEN "
+            + "            TIMESTAMPDIFF(DAY, "
+            + "                e.prev_drug_pickup_scheduled_date_fila, "
+            + "                e.value_datetime) "
+            + "        WHEN "
+            + "            prev_drug_pickup_scheduled_date_fila IS NULL "
+            + "                AND prev_drug_pickup_scheduled_date_master_card IS NOT NULL "
+            + "        THEN "
+            + "            TIMESTAMPDIFF(DAY, "
+            + "                e.prev_drug_pickup_scheduled_date_master_card, "
+            + "                e.value_datetime) "
+            + "    END > 28 "
+            + "        OR CASE "
+            + "        WHEN "
+            + "            prev_drug_pickup_scheduled_date_fila IS NOT NULL "
+            + "                AND prev_drug_pickup_scheduled_date_master_card IS NOT NULL "
+            + "        THEN "
+            + "            CASE "
+            + "                WHEN "
+            + "                    TIMESTAMPDIFF(DAY, "
+            + "                        e.prev_drug_pickup_scheduled_date_fila, "
+            + "                        e.value_datetime) > 28 "
+            + "                        AND TIMESTAMPDIFF(DAY, "
+            + "                        e.prev_drug_pickup_scheduled_date_master_card, "
+            + "                        e.value_datetime) > 28 "
+            + "                THEN "
+            + "                    57 "
+            + "                ELSE 45 "
+            + "            END "
+            + "    END > 56)) "
+            + "        OR (e.encounter_type !=  ${masterCardEncounter} "
+            + "        AND e.encounter_datetime BETWEEN :startDate AND :endDate "
+            + "        AND TIMESTAMPDIFF(DAY, "
+            + "        e.prev_consultation_scheduled_date, "
+            + "        e.encounter_datetime) > 28 "
+            + "        AND (CASE "
+            + "        WHEN "
+            + "            prev_drug_pickup_scheduled_date_fila IS NOT NULL "
+            + "                AND prev_drug_pickup_scheduled_date_master_card IS NULL "
+            + "        THEN "
+            + "            TIMESTAMPDIFF(DAY, "
+            + "                e.prev_drug_pickup_scheduled_date_fila, "
+            + "                e.encounter_datetime) "
+            + "        WHEN "
+            + "            prev_drug_pickup_scheduled_date_fila IS NULL "
+            + "                AND prev_drug_pickup_scheduled_date_master_card IS NOT NULL "
+            + "        THEN "
+            + "            TIMESTAMPDIFF(DAY, "
+            + "                e.prev_drug_pickup_scheduled_date_master_card, "
+            + "                e.encounter_datetime) "
+            + "    END > 28 "
+            + "        OR CASE "
+            + "        WHEN "
+            + "            prev_drug_pickup_scheduled_date_fila IS NOT NULL "
+            + "                AND prev_drug_pickup_scheduled_date_master_card IS NOT NULL "
+            + "        THEN "
+            + "            CASE "
+            + "                WHEN "
+            + "                    TIMESTAMPDIFF(DAY, "
+            + "                        e.prev_drug_pickup_scheduled_date_fila, "
+            + "                        e.encounter_datetime) > 28 "
+            + "                        AND TIMESTAMPDIFF(DAY, "
+            + "                        e.prev_drug_pickup_scheduled_date_master_card, "
+            + "                        e.encounter_datetime) > 28 "
+            + "                THEN "
+            + "                    57 "
+            + "                ELSE 45 "
+            + "            END "
+            + "    END > 56)) "
+            + "GROUP BY patient_id";
+
+    Map<String, Integer> map = new HashMap<>();
+    map.put("adultoSegEncounter", adultoSegEncounter);
+    map.put("pediatriaSegEncounter", pediatriaSegEncounter);
+    map.put("pharmacyEncounter", pharmacyEncounter);
+    map.put("masterCardEncounter", masterCardEncounter);
+    map.put("consultationConcept", consultationConcept);
+    map.put("nextDrugPickupConcept", nextDrugPickupConcept);
+    map.put("dateOfArtPickupConceptMasterCard", dateOfArtPickupConceptMasterCard);
+
+    StringSubstitutor sub = new StringSubstitutor(map);
+    return sub.replace(query);
   }
 }
