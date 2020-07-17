d10b03647d5898d6da96b9d61961fa45dbb86019
Amós <49514817+amosh101@users.noreply.github.com>
2020-07-17 16:32:53 +0200

    MEPTS-581: Add Mastercard Ficha Resumo CD4 evaluation (#173)
    * Add Mastercard Cd4 evaluation
    
    * return to private

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/EriDSDCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/EriDSDCohortQueries.java
@@ -317,8 +317,21 @@ public class EriDSDCohortQueries {
         EptsReportUtils.map(
             ageCohortQueries.createXtoYAgeCohort("greaterThan5", 5, 900),
             "effectiveDate=${endDate}"));
+            cd.addSearch(
+        "B",
+        EptsReportUtils.map(
+            genericCohortQueries.hasNumericObs(
+                hivMetadata.getCD4AbsoluteOBSConcept(),
+                BaseObsCohortDefinition.TimeModifier.LAST,
+                RangeComparator.GREATER_THAN,
+                200.0,
+                null,
+                null,
+                Arrays.asList(
+                    hivMetadata.getMasterCardEncounterType())),
+            "onOrAfter=${endDate-12m},onOrBefore=${endDate},locationList=${location}"));
 
-    cd.setCompositionString("((CD4Abs OR Cd4Lab) AND Age)");
+    cd.setCompositionString("((CD4Abs OR Cd4Lab OR B) AND Age)");
 
     return cd;
   }
