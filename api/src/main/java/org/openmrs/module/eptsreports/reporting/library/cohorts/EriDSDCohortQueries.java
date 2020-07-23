213ddd085e1ccd2c5e61ddc14a8fc1b2c35a3e2f
Yassin <yassin791@gmail.com>
2020-07-23 08:47:05 +0200

    MEPTS-598: Remove composition and use only onOrBefore parameter (#193)

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/EriDSDCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/EriDSDCohortQueries.java
@@ -22,6 +22,7 @@ import org.openmrs.module.reporting.cohort.definition.BaseObsCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CodedObsCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CompositionCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.MappedParametersCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.SqlCohortDefinition;
 import org.openmrs.module.reporting.common.RangeComparator;
 import org.openmrs.module.reporting.common.SetComparator;
@@ -282,20 +283,18 @@ public class EriDSDCohortQueries {
 
   /** N7 - Number of active patients on ART who are in DC */
   public CohortDefinition getN7() {
-    CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("N7 - Active patients in ART marked in last DC as start or continue regimen");
-    cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
-    cd.addParameter(new Parameter("endDate", "End Date", Date.class));
-    cd.addParameter(new Parameter("location", "Location", Location.class));
-
-    CohortDefinition communityDispense = getPatientsMarkedInLastCommunityDispense();
-
-    String dspMappings = "onOrAfter=${startDate},onOrBefore=${endDate},locationList=${location}";
-    cd.addSearch("communityDispense", EptsReportUtils.map(communityDispense, dspMappings));
-
-    cd.setCompositionString("communityDispense");
-
-    return cd;
+    CodedObsCohortDefinition cd1 = new CodedObsCohortDefinition();
+    cd1.setName("N7 - Active patients in ART marked in last DC as start or continue regimen");
+    cd1.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+    cd1.addParameter(new Parameter("locationList", "Location", Location.class));
+    cd1.addEncounterType(hivMetadata.getAdultoSeguimentoEncounterType());
+    cd1.setTimeModifier(BaseObsCohortDefinition.TimeModifier.LAST);
+    cd1.setQuestion(hivMetadata.getCommunityDispensation());
+    cd1.setOperator(SetComparator.IN);
+    cd1.addValue(hivMetadata.getStartDrugs());
+    cd1.addValue(hivMetadata.getContinueRegimenConcept());
+    return new MappedParametersCohortDefinition(
+        cd1, "endDate", "onOrBefore", "location", "locationList");
   }
 
   /** N8 - Number of active patients on ART who participate in at least one DSD model */
@@ -807,21 +806,6 @@ public class EriDSDCohortQueries {
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
     cd.addParameter(new Parameter("location", "Location", Location.class));
     cd.setQuery(DsdQueries.getPatientsEnrolledOnGAAC());
-    return cd;
-  }
-
-  /** Patients marked in last Dispensa Comunitaria as start or continue regimen query */
-  private CohortDefinition getPatientsMarkedInLastCommunityDispense() {
-    CodedObsCohortDefinition cd = new CodedObsCohortDefinition();
-    cd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
-    cd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
-    cd.addParameter(new Parameter("locationList", "Location", Location.class));
-    cd.addEncounterType(hivMetadata.getAdultoSeguimentoEncounterType());
-    cd.setTimeModifier(BaseObsCohortDefinition.TimeModifier.LAST);
-    cd.setQuestion(hivMetadata.getCommunityDispensation());
-    cd.setOperator(SetComparator.IN);
-    cd.addValue(hivMetadata.getStartDrugs());
-    cd.addValue(hivMetadata.getContinueRegimenConcept());
     return cd;
   }
 
