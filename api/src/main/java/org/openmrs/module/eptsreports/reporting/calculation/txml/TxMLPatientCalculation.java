5e5660d1007d49f3c2ec8a26fb9a78171a62572d
guimino <minoneves@gmail.com>
2020-08-13 13:13:06 +0200

    EH-151: Tx_ML update homevisit search rule

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/txml/TxMLPatientCalculation.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/txml/TxMLPatientCalculation.java
@@ -23,8 +23,6 @@ import org.openmrs.module.reporting.evaluation.EvaluationContext;
 
 public abstract class TxMLPatientCalculation extends BaseFghCalculation {
 
-  public static int DAYS_TO_LTFU = 28;
-
   @Override
   public CalculationResultMap evaluate(
       Map<String, Object> parameterValues, EvaluationContext context) {
@@ -140,9 +138,7 @@ public abstract class TxMLPatientCalculation extends BaseFghCalculation {
         if (numeratorNextExpectedDate != null) {
           Date candidateDate = deadInHomeVisitForm.get(patientId);
           if (candidateDate != null) {
-            Date nextDatePlus28 =
-                CalculationProcessorUtils.adjustDaysInDate(numeratorNextExpectedDate, DAYS_TO_LTFU);
-            if (candidateDate.compareTo(nextDatePlus28) > 0) {
+            if (candidateDate.compareTo(numeratorNextExpectedDate) > 0) {
               result.put(patientId, candidateDate);
             }
           }
