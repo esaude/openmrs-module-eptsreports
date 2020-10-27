bc5ff19fd17fa0311e60396f429024c581504f5d
guimino <minoneves@gmail.com>
2020-10-27 08:55:59 +0200

    REPORT-159: Misau Relatorio Populacao chave

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/MisauKeyPopDataSetDefinition.java
@@ -0,0 +1,168 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.eptsreports.reporting.library.datasets;
+
+import static org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils.map;
+
+import org.openmrs.module.eptsreports.reporting.library.cohorts.ResumoMensalCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.TxCurrCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.TxNewCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.AgeDimensionCohortInterface;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.EptsCommonDimension;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.KeyPopulationDimension;
+import org.openmrs.module.eptsreports.reporting.library.disaggregations.ResumoMensalAandBdisaggregations;
+import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.indicator.CohortIndicator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MisauKeyPopDataSetDefinition extends BaseDataSet {
+
+  private EptsCommonDimension eptsCommonDimension;
+
+  private EptsGeneralIndicator eptsGeneralIndicator;
+  private KeyPopulationDimension keyPopulationDimension;
+
+  private TxNewCohortQueries txNewCohortQueries;
+
+  private TxCurrCohortQueries txCurrCohortQueries;
+
+  @Autowired
+  @Qualifier("commonAgeDimensionCohort")
+  private AgeDimensionCohortInterface ageDimensionCohort;
+
+  @Autowired
+  public MisauKeyPopDataSetDefinition(
+      EptsCommonDimension eptsCommonDimension,
+      EptsGeneralIndicator eptsGeneralIndicator,
+      ResumoMensalCohortQueries resumoMensalCohortQueries,
+      ResumoMensalAandBdisaggregations resumoMensalAandBdisaggregations,
+      TxNewCohortQueries txNewCohortQueries,
+      TxCurrCohortQueries txCurrCohortQueries,
+      KeyPopulationDimension keyPopulationDimension) {
+    this.eptsCommonDimension = eptsCommonDimension;
+    this.eptsGeneralIndicator = eptsGeneralIndicator;
+    this.txNewCohortQueries = txNewCohortQueries;
+    this.txCurrCohortQueries = txCurrCohortQueries;
+    this.keyPopulationDimension = keyPopulationDimension;
+  }
+
+  public DataSetDefinition constructMisauKeyPopDataset() {
+    CohortIndicatorDataSetDefinition dataSetDefinition = new CohortIndicatorDataSetDefinition();
+
+    dataSetDefinition.setName("Misau Key Pop Dataset");
+    dataSetDefinition.addParameters(getParameters());
+    final String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
+    final String mappingEndDate = "endDate=${endDate},location=${location}";
+
+    final CohortDefinition patientEnrolledInART =
+        this.txNewCohortQueries.getTxNewCompositionCohort("patientEnrolledInART");
+    final CohortIndicator patientEnrolledInHIVStartedARTIndicator =
+        this.eptsGeneralIndicator.getIndicator(
+            "patientNewlyEnrolledInHIVIndicator",
+            EptsReportUtils.map(patientEnrolledInART, mappings));
+
+    final CohortDefinition patientCurrentOnART =
+        this.txCurrCohortQueries.findPatientsWhoAreActiveOnART();
+
+    final CohortIndicator patientCurrentOnARTIndicator =
+        this.eptsGeneralIndicator.getIndicator(
+            "findPatientsWhoAreActiveOnART",
+            EptsReportUtils.map(patientCurrentOnART, mappingEndDate));
+
+    dataSetDefinition.addDimension("gender", map(eptsCommonDimension.gender(), ""));
+    dataSetDefinition.addDimension(
+        "age", map(eptsCommonDimension.age(ageDimensionCohort), "effectiveDate=${endDate}"));
+    dataSetDefinition.addDimension(
+        "homosexual",
+        EptsReportUtils.map(this.keyPopulationDimension.findPatientsWhoAreHomosexual(), mappings));
+    dataSetDefinition.addDimension(
+        "drug-user",
+        EptsReportUtils.map(this.keyPopulationDimension.findPatientsWhoUseDrugs(), mappings));
+    dataSetDefinition.addDimension(
+        "prisioner",
+        EptsReportUtils.map(this.keyPopulationDimension.findPatientsWhoAreInPrison(), mappings));
+    dataSetDefinition.addDimension(
+        "sex-worker",
+        EptsReportUtils.map(this.keyPopulationDimension.findPatientsWhoAreSexWorker(), mappings));
+
+    dataSetDefinition.addColumn(
+        "E1",
+        "Numero adultos que iniciaram TARV - Total",
+        EptsReportUtils.map(patientEnrolledInHIVStartedARTIndicator, mappings),
+        "age=15+");
+
+    dataSetDefinition.addColumn(
+        "E1-MSM",
+        "Homosexual",
+        EptsReportUtils.map(patientEnrolledInHIVStartedARTIndicator, mappings),
+        "age=15+|homosexual=homosexual");
+
+    dataSetDefinition.addColumn(
+        "E1-PWID",
+        "Drugs User",
+        EptsReportUtils.map(patientEnrolledInHIVStartedARTIndicator, mappings),
+        "age=15+|drug-user=drug-user");
+
+    dataSetDefinition.addColumn(
+        "E1-PRI",
+        "Prisioners",
+        EptsReportUtils.map(patientEnrolledInHIVStartedARTIndicator, mappings),
+        "age=15+|prisioner=prisioner");
+
+    dataSetDefinition.addColumn(
+        "E1-FSW",
+        "Sex Worker",
+        EptsReportUtils.map(patientEnrolledInHIVStartedARTIndicator, mappings),
+        "age=15+|sex-worker=sex-worker");
+
+    dataSetDefinition.addColumn(
+        "E2",
+        "Numero adultos actualmente em TARV - Total",
+        EptsReportUtils.map(patientCurrentOnARTIndicator, mappingEndDate),
+        "age=15+");
+
+    dataSetDefinition.addColumn(
+        "E2-MSM",
+        "Homosexual",
+        EptsReportUtils.map(patientCurrentOnARTIndicator, mappingEndDate),
+        "age=15+|homosexual=homosexual");
+
+    dataSetDefinition.addColumn(
+        "E2-PWID",
+        "Drugs User",
+        EptsReportUtils.map(patientCurrentOnARTIndicator, mappingEndDate),
+        "age=15+|drug-user=drug-user");
+
+    dataSetDefinition.addColumn(
+        "E2-PRI",
+        "Prisioners",
+        EptsReportUtils.map(patientCurrentOnARTIndicator, mappingEndDate),
+        "age=15+|prisioner=prisioner");
+
+    dataSetDefinition.addColumn(
+        "E2-FSW",
+        "Sex Worker",
+        EptsReportUtils.map(patientCurrentOnARTIndicator, mappingEndDate),
+        "age=15+|sex-worker=sex-worker");
+
+    return dataSetDefinition;
+  }
+}
---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMisauKeyPopReport.java
@@ -0,0 +1,106 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.eptsreports.reporting.reports;
+
+import static org.openmrs.module.reporting.evaluation.parameter.Mapped.mapStraightThrough;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.datasets.MisauKeyPopDataSetDefinition;
+import org.openmrs.module.eptsreports.reporting.library.queries.BaseQueries;
+import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
+import org.openmrs.module.reporting.ReportingException;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetupMisauKeyPopReport extends EptsDataExportManager {
+
+  private MisauKeyPopDataSetDefinition misauKeyPopDataSetDefinition;
+
+  @Autowired protected GenericCohortQueries genericCohortQueries;
+
+  @Autowired
+  public SetupMisauKeyPopReport(MisauKeyPopDataSetDefinition misauKeyPopDataSetDefinition) {
+    this.misauKeyPopDataSetDefinition = misauKeyPopDataSetDefinition;
+  }
+
+  @Override
+  public String getExcelDesignUuid() {
+    return "9d9afa2f-d107-48e7-bd0a-c82a24fecdec";
+  }
+
+  @Override
+  public String getUuid() {
+    return "5de5a817-000e-4572-80a1-61ab83a49461";
+  }
+
+  @Override
+  public String getName() {
+    return "Relatorio de Populacao Chave - MISAU TEST";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Relatorio de Populacao Chave - MISAU TEST";
+  }
+
+  @Override
+  public ReportDefinition constructReportDefinition() {
+    ReportDefinition rd = new ReportDefinition();
+    rd.setUuid(getUuid());
+    rd.setName(getName());
+    rd.setDescription(getDescription());
+    rd.addParameters(misauKeyPopDataSetDefinition.getParameters());
+    rd.addDataSetDefinition(
+        "M", mapStraightThrough(misauKeyPopDataSetDefinition.constructMisauKeyPopDataset()));
+    rd.setBaseCohortDefinition(
+        EptsReportUtils.map(
+            this.genericCohortQueries.generalSql(
+                "baseCohortQuery", BaseQueries.getBaseCohortQuery()),
+            "endDate=${endDate},location=${location}"));
+    return rd;
+  }
+
+  @Override
+  public String getVersion() {
+    return "1.0-SNAPSHOT";
+  }
+
+  @Override
+  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+    ReportDesign reportDesign = null;
+    try {
+      reportDesign =
+          createXlsReportDesign(
+              reportDefinition,
+              "Relatorio_de_Populacao_Chave_MISAU.xls",
+              "Relatorio de Populacao Chave - MISAU",
+              getExcelDesignUuid(),
+              null);
+      Properties props = new Properties();
+      props.put("sortWeight", "5000");
+      reportDesign.setProperties(props);
+    } catch (IOException e) {
+      throw new ReportingException(e.toString());
+    }
+    return Arrays.asList(reportDesign);
+  }
+}
