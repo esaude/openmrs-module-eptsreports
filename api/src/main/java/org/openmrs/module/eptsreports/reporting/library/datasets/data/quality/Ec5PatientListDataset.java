ff042643fc859def80809857cfbe08ba84e74d1a
emaposse <eusebiomaposse@gmail.com>
2020-11-17 23:10:47 +0200

    Add EC5 Indicator Definition.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/data/quality/Ec5PatientListDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/data/quality/Ec5PatientListDataset.java
@@ -14,34 +14,22 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets.data.quality;
 
 import java.util.List;
-import org.openmrs.module.eptsreports.metadata.HivMetadata;
 import org.openmrs.module.eptsreports.reporting.library.datasets.BaseDataSet;
 import org.openmrs.module.eptsreports.reporting.library.queries.data.quality.Ec5Queries;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ec5PatientListDataset extends BaseDataSet {
 
-  private HivMetadata hivMetadata;
-
-  @Autowired
-  public Ec5PatientListDataset(HivMetadata hivMetadata) {
-    this.hivMetadata = hivMetadata;
-  }
-
   public DataSetDefinition ec5PatientListDataset(List<Parameter> parameterList) {
     SqlDataSetDefinition dsd = new SqlDataSetDefinition();
     dsd.setName("EC5");
     dsd.addParameters(parameterList);
-    dsd.setSqlQuery(
-        Ec5Queries.getEc5CombinedQuery(
-            hivMetadata.getARTProgram().getProgramId(),
-            hivMetadata.getArtDeadWorkflowState().getProgramWorkflowStateId(),
-            hivMetadata.getMisauLaboratorioEncounterType().getEncounterTypeId()));
+    dsd.setSqlQuery(Ec5Queries.getEc5CombinedQuery());
+
     return dsd;
   }
 }
