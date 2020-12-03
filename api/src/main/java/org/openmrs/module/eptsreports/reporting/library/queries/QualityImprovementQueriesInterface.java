ece34b6baf9a8d4f872757e49dbef9b04baf53a7
emaposse <eusebiomaposse@gmail.com>
2020-12-03 19:49:06 +0200

    Remove some issue on query.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/QualityImprovementQueriesInterface.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/QualityImprovementQueriesInterface.java
@@ -91,7 +91,7 @@ public interface QualityImprovementQueriesInterface {
                 + "INNER JOIN encounter e ON p.patient_id=e.patient_id "
                 + "INNER JOIN obs o ON e.encounter_id=o.encounter_id "
                 + "WHERE p.voided=0 AND e.voided=0 AND o.voided=0 AND e.encounter_type=53 "
-                + "AND o.concept_id=1190 AND o.value_datetime is NOT NULL AND o.value_datetime<=:endDate AND e.location_id=:location "
+                + "AND o.concept_id=1190 AND o.value_datetime is NOT NULL AND o.value_datetime<=:endInclusionDate AND e.location_id=:location "
                 + "GROUP BY p.patient_id "
                 + "UNION "
                 + "Select p.patient_id,min(o.value_datetime) art_start_date from  patient p "
@@ -99,7 +99,7 @@ public interface QualityImprovementQueriesInterface {
                 + "inner join obs o on e.encounter_id=o.encounter_id "
                 + "inner join obs oLevantou on e.encounter_id=oLevantou.encounter_id "
                 + "where  p.voided=0 and e.voided=0 and o.voided=0 and oLevantou.voided=0 and e.encounter_type=52 and o.concept_id=23866 and "
-                + "o.value_datetime is not null and o.value_datetime<=:endDate and e.location_id=:location and "
+                + "o.value_datetime is not null and o.value_datetime<=:endInclusionDate and e.location_id=:location and "
                 + "oLevantou.concept_id=23865 and oLevantou.value_coded=1065 "
                 + "group by p.patient_id "
                 + ") art_start GROUP "
@@ -107,7 +107,7 @@ public interface QualityImprovementQueriesInterface {
                 + ") tx_new "
                 + "INNER JOIN person pe ON tx_new.patient_id=pe.person_id "
                 + "WHERE (TIMESTAMPDIFF(year,birthdate,art_start_date)/365)>=15 AND birthdate IS NOT NULL and pe.voided=0 "
-                + "AND art_start_date BETWEEN :startDate AND :endDate ";
+                + "AND art_start_date BETWEEN :startInclusionDate AND :endInclusionDate ";
 
     public static final String
         findPatientsWhoAreNewlyEnrolledOnARTByAgeUsingYearChildrenDesagragation =
