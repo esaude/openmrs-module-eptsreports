4317c505f76ce4e43c11cf098425fc5d598ff8b6
emaposse <eusebiomaposse@gmail.com>
2020-12-10 13:24:21 +0200

    Add Quality Improvment Class Calculation

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/quality/improvment/calculation/QualityImprovmentCalculationCategory11.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.eptsreports.reporting.library.quality.improvment.calculation;
+
+import org.openmrs.module.eptsreports.reporting.calculation.BaseFghCalculation;
+
+public class QualityImprovmentCalculationCategory11 extends BaseFghCalculation{
+
+}
