a2928053603c74b87fde94154e0ce044d826a03a
Paulo Sérgio Jaime Matsinhe <psmatsinhe@gmail.com>
2020-12-30 12:38:31 +0200

    MEPTS: fix basecohort date (#350)

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupQualityImprovementReport20.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupQualityImprovementReport20.java
@@ -63,7 +63,7 @@ public class SetupQualityImprovementReport20 extends EptsDataExportManager {
     // add a base cohort here to help in calculations running
     reportDefinition.setBaseCohortDefinition(
         EptsReportUtils.map(
-            genericCohortQueries.getBaseCohort(), "endDate=${endDate},location=${location}"));
+            genericCohortQueries.getBaseCohort(), "endDate=${revisionEndDate},location=${location}"));
 
     return reportDefinition;
   }
