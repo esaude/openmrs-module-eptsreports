c5f7a93bab95d00e871276a68eb6071df1edb0ab
emaposse <eusebiomaposse@gmail.com>
2021-02-25 10:02:07 +0200

    fix query.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TxTbPrevExclusions.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TxTbPrevExclusions.java
@@ -12,7 +12,7 @@ public class TxTbPrevExclusions {
             + "inner join encounter e on p.patient_id=e.patient_id  "
             + "inner join obs o on o.encounter_id=e.encounter_id  "
             + "where e.voided=0 and p.voided=0  and e.patient_id=:patientId and e.encounter_datetime between (:encounterDate) and (:encounterDate + interval 7 month) "
-            + "and o.voided=0 and o.concept_id=1719 and e.encounter_type=6  and o.value_coded in(23954) and e.location_id=:location  "
+            + "and o.voided=0 and o.concept_id=1719 and e.encounter_type=6  and o.value_coded in(23954,23955) and e.location_id=:location  "
             + "group by p.patient_id "
             + ")  enc  ";
   }
