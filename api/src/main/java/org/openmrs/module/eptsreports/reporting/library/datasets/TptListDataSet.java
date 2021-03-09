1b110ad30e35e0700f7500ea210c31271516955e
emaposse <eusebiomaposse@gmail.com>
2021-03-09 11:18:26 +0200

    Add TPT indicator Report.

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TptListDataSet.java
@@ -0,0 +1,20 @@
+package org.openmrs.module.eptsreports.reporting.library.datasets;
+
+import java.util.List;
+import org.openmrs.module.eptsreports.reporting.library.queries.List1Queries;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TptListDataSet extends BaseDataSet {
+
+  public DataSetDefinition eTptDataSetDefinition(List<Parameter> parameterList) {
+    SqlDataSetDefinition dsd = new SqlDataSetDefinition();
+    dsd.setName("INICIOTPI");
+    dsd.addParameters(parameterList);
+    dsd.setSqlQuery(List1Queries.QUERY.finPatientsWhoInitietedINH);
+    return dsd;
+  }
+}
---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTptReport.java
@@ -0,0 +1,112 @@
+package org.openmrs.module.eptsreports.reporting.reports;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.openmrs.Location;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.datasets.TptListDataSet;
+import org.openmrs.module.eptsreports.reporting.library.datasets.TptTotalDataseet;
+import org.openmrs.module.eptsreports.reporting.library.queries.BaseQueries;
+import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.ReportingException;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetupTptReport extends EptsDataExportManager {
+
+  @Autowired private TptListDataSet tptListDataSet;
+  @Autowired private TptTotalDataseet tptTotalDataseet;
+  @Autowired protected GenericCohortQueries genericCohortQueries;
+
+  @Override
+  public String getExcelDesignUuid() {
+    return "65e364d6-9be0-11e9-a7a2-cb23750cc2d0";
+  }
+
+  @Override
+  public String getUuid() {
+    return "82ea2326-9be0-11e9-9cee-5384d0247691";
+  }
+
+  @Override
+  public String getName() {
+    return "LISTA DE PACIENTES QUE INICIARAM PROFILAXIA COM ISONIAZIDA (TPI)";
+  }
+
+  @Override
+  public String getDescription() {
+    return "LISTA DE PACIENTES QUE INICIARAM PROFILAXIA COM ISONIAZIDA (TPI)";
+  }
+
+  @Override
+  public ReportDefinition constructReportDefinition() {
+    ReportDefinition rd = new ReportDefinition();
+    rd.setUuid(getUuid());
+    rd.setName(getName());
+    rd.setDescription(getDescription());
+    rd.addParameters(getDataParameters());
+
+    rd.addDataSetDefinition(
+        "INICIOTPI",
+        Mapped.mapStraightThrough(tptListDataSet.eTptDataSetDefinition(getDataParameters())));
+
+    rd.addDataSetDefinition(
+        "TPI", Mapped.mapStraightThrough(this.tptTotalDataseet.constructDataset()));
+
+    rd.setBaseCohortDefinition(
+        EptsReportUtils.map(
+            this.genericCohortQueries.generalSql(
+                "baseCohortQuery", BaseQueries.getBaseCohortQuery()),
+            "endDate=${endDate},location=${location}"));
+
+    return rd;
+  }
+
+  @Override
+  public String getVersion() {
+    return "1.0-SNAPSHOT";
+  }
+
+  @Override
+  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+    ReportDesign reportDesign = null;
+    try {
+      reportDesign =
+          createXlsReportDesign(
+              reportDefinition,
+              "TPT.xls",
+              "TPT",
+              getExcelDesignUuid(),
+              null);
+      Properties props = new Properties();
+      props.put("repeatingSections", "sheet:1,row:4,dataset:INICIOTPI");
+
+      props.put("sortWeight", "5000");
+      props.put("sortWeight", "5000");
+      reportDesign.setProperties(props);
+    } catch (IOException e) {
+      throw new ReportingException(e.toString());
+    }
+
+    return Arrays.asList(reportDesign);
+  }
+
+  private List<Parameter> getDataParameters() {
+    List<Parameter> parameters = new ArrayList<Parameter>();
+    parameters.add(ReportingConstants.START_DATE_PARAMETER);
+    parameters.add(ReportingConstants.END_DATE_PARAMETER);
+    parameters.add(new Parameter("location", "Facilities", Location.class, List.class, null));
+    return parameters;
+  }
+}
