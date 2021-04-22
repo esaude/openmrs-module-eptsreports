228aedbc35d2239545a7ef6ca496dff392ac5a8f
Jose Chambule <engchambule@gmail.com>
2021-04-22 21:57:47 +0200

    CAT11 CERTIFICATION

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/GenericMQQueryIntarface.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/GenericMQQueryIntarface.java
@@ -116,7 +116,7 @@ public interface GenericMQQueryIntarface {
               + "group by p.patient_id "
               + ") carga_viral "
               + "inner join person on person_id = carga_viral.patient_id "
-              + "WHERE (TIMESTAMPDIFF(year, birthdate, carga_viral.data_carga)) > %s  AND birthdate IS NOT NULL and voided = 0  ";
+              + "WHERE (TIMESTAMPDIFF(year, birthdate, carga_viral.data_carga)) >= %s  AND birthdate IS NOT NULL and voided = 0  ";
 
       return String.format(sql, startAge);
     }
