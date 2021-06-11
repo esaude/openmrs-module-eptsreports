5349ba541dbf38ebf6d639dfd1fc95439c148b7b
guimino <minoneves@gmail.com>
2021-06-11 18:00:06 +0200

    added Data Quality Duplicated

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/data/quality/duplicate/EC2DuplicateQueries.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.eptsreports.reporting.library.queries.data.quality.duplicate;
+
+public interface EC2DuplicateQueries {
+  class QUERY {
+
+    public static String findPatiendsWithDuplicatedNID =
+        "                                                                                                               			"
+            + "select distinct  																																					"
+            + "	patient.patient_id, 																													 							"
+            + "     location.name locationName,  																																	"
+            + "     patient_identifier.patient_identifier_id,   																										 			"
+            + "     patient_identifier.identifier,    	 																											 	 			"
+            + "     concat(ifnull(person_name.given_name,''),' ',ifnull(person_name.middle_name,''),' ',ifnull(person_name.family_name,'')) as nomeCompleto, 	 	 				"
+            + "     person.birthdate, 							 																								 	 				"
+            + "     if(person.birthdate_estimated = true, 'SIM','NAO') birthdate_estimated,   															 		 	 				"
+            + "     person.gender , 																 															 	 				"
+            + "    	patient.date_created, 																 														 	 				"
+            + "     patient.date_changed, 																														 	 				"
+            + "    	patient_identifier.date_created dataCriacaoNID, 																							 	 				"
+            + "     if(patient_identifier.preferred = true, 'SIM','NAO') nidPreferido, 																			 	 				"
+            + "     patient_program.date_enrolled	  																																"
+            + "from 																																				 	 			"
+            + "(	select patient.patient_id from patient 																												 			"
+            + "		join patient_identifier on patient_identifier.patient_id = patient.patient_id 																		 			"
+            + "	where patient.voided =0 and patient_identifier.voided =0 and patient_identifier.identifier_type =2 													 				"
+            + "		group by patient.patient_id having count(patient_identifier.identifier) >=2																			 			"
+            + ") paientID																																		 	 				"
+            + "	inner join patient_identifier on patient_identifier.patient_id = paientID.patient_id 																 				"
+            + "	inner join patient on patient.patient_id =patient_identifier.patient_id   																							"
+            + "	inner join person  on (person.person_id =patient.patient_id and person.voided =0)  																					"
+            + "	inner join person_name on (person_name.person_id = patient.patient_id and person_name.voided =0)		 															"
+            + "	left join location on (location.location_id =patient_identifier.location_id and location.retired =0) 																"
+            + "	left join patient_program on (patient_program.patient_id = patient.patient_id and patient_program.program_id =2 and patient_program.voided =0) 						"
+            + "group by patient_identifier.patient_identifier_id order by patient.patient_id  																						";
+
+    public static String getEc2Total =
+        "                                                 								"
+            + "select distinct patient.patient_id																    "
+            + "from 																								    "
+            + "(select patient.patient_id from patient 															    "
+            + " 	join patient_identifier on patient_identifier.patient_id = patient.patient_id 						"
+            + " 	where patient.voided =0 and patient_identifier.voided =0 and patient_identifier.identifier_type =2  "
+            + " 	group by patient.patient_id having count(patient_identifier.identifier) >=2							"
+            + " ) paientID																							"
+            + " inner join patient_identifier on patient_identifier.patient_id = paientID.patient_id 				"
+            + " inner join patient on patient.patient_id =patient_identifier.patient_id 								";
+  }
+}
---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupDataQualityDuplicateReport.java
@@ -0,0 +1,110 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.eptsreports.reporting.reports;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import org.openmrs.module.eptsreports.reporting.library.datasets.data.quality.duplicate.EC1PatientListDuplicateDataset;
+import org.openmrs.module.eptsreports.reporting.library.datasets.data.quality.duplicate.EC2PatientListDuplicateDataset;
+import org.openmrs.module.eptsreports.reporting.library.datasets.data.quality.duplicate.SummaryDataQualityDuplicateDataset;
+import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
+import org.openmrs.module.reporting.ReportingException;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetupDataQualityDuplicateReport extends EptsDataExportManager {
+
+  @Autowired private SummaryDataQualityDuplicateDataset summaryDataQualityDuplicateDataset;
+
+  @Autowired private EC1PatientListDuplicateDataset ec1PatientListDuplicateDataset;
+
+  @Autowired private EC2PatientListDuplicateDataset ec2PatientListDuplicateDataset;
+
+  @Override
+  public String getExcelDesignUuid() {
+    return "3784716d-c6a5-49ca-b25c-14f804eb4f6e";
+  }
+
+  @Override
+  public String getUuid() {
+    return "20e14afe-16c4-4a38-92cb-b548726c5428";
+  }
+
+  @Override
+  public String getName() {
+    return "RELATÓRIO DE DADOS PARA POTENCIAIS DUPLICAÇÕES";
+  }
+
+  @Override
+  public String getDescription() {
+    return "This report provides a line listing of patient  records failing to meet certain edit checks and allows the user to review the information so the patient’s information can be corrected";
+  }
+
+  @Override
+  public ReportDefinition constructReportDefinition() {
+    ReportDefinition rd = new ReportDefinition();
+    rd.setUuid(getUuid());
+    rd.setName(getName());
+    rd.setDescription(getDescription());
+
+    rd.addDataSetDefinition(
+        "SD",
+        Mapped.mapStraightThrough(
+            summaryDataQualityDuplicateDataset.constructSummaryDataQualityDatset()));
+
+    rd.addDataSetDefinition(
+        "ECD1",
+        Mapped.mapStraightThrough(ec1PatientListDuplicateDataset.ec1PatientDuplicateListDataset()));
+
+    rd.addDataSetDefinition(
+        "ECD2",
+        Mapped.mapStraightThrough(ec2PatientListDuplicateDataset.ec1PatientDuplicateListDataset()));
+    return rd;
+  }
+
+  @Override
+  public String getVersion() {
+    return "1.0-SNAPSHOT";
+  }
+
+  @Override
+  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+    ReportDesign reportDesign = null;
+    try {
+      reportDesign =
+          createXlsReportDesign(
+              reportDefinition,
+              "Data_Quality_Duplicates_Report.xls",
+              "Data Quality Duplicate Report",
+              getExcelDesignUuid(),
+              null);
+      Properties props = new Properties();
+      props.put("repeatingSections", "sheet:2,row:7,dataset:ECD1 | sheet:3,row:7,dataset:ECD2");
+      props.put("sortWeight", "5000");
+      props.put("sortWeight", "5000");
+      reportDesign.setProperties(props);
+    } catch (IOException e) {
+      throw new ReportingException(e.toString());
+    }
+
+    return Arrays.asList(reportDesign);
+  }
+}
