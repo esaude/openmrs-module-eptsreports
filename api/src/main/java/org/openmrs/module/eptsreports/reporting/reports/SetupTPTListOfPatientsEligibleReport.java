f92242b9c0b56b815fa25c7fa3549a8c9ed86330
rupansana12 <54063169+rupansana12@users.noreply.github.com>
2021-06-14 10:24:54 +0200

    Fix startDate and Location in templete. (#563)

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTPTListOfPatientsEligibleReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTPTListOfPatientsEligibleReport.java
@@ -15,8 +15,9 @@ import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Deprecated
+@Component
 public class SetupTPTListOfPatientsEligibleReport extends EptsDataExportManager {
 
   private TPTListOfPatientsEligibleDataSet tptListOfPatientsEligibleDataSet;
