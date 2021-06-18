82b7ed3e66b889d39bcea607d4307cdd6b1d4167
guimino <minoneves@gmail.com>
2021-06-18 12:30:04 +0200

    code formatted

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/TxRTTCalculationTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/TxRTTCalculationTest.java
@@ -23,81 +23,80 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class TxRTTCalculationTest extends DefinitionsFGHLiveTest {
 
-	@Autowired
-	private TxRTTCohortQueries txRTTCohortQueries;
+  @Autowired private TxRTTCohortQueries txRTTCohortQueries;
 
-	@Autowired
-	private ResumoTrimestralAPSSCohortQueries resumoMensalAPSSCohortQueries;
+  @Autowired private ResumoTrimestralAPSSCohortQueries resumoMensalAPSSCohortQueries;
 
-	@Autowired
-	private TXTBCohortQueries txTBCohortQueries;
+  @Autowired private TXTBCohortQueries txTBCohortQueries;
 
-	@Autowired
-	private TPTCompletationCohortQueries tptCompletationCohortQueries;
+  @Autowired private TPTCompletationCohortQueries tptCompletationCohortQueries;
 
-	@Test
-	public void shouldFindPatientsNewlyEnrolledInART() throws EvaluationException {
+  @Test
+  public void shouldFindPatientsNewlyEnrolledInART() throws EvaluationException {
 
-		final Location location = Context.getLocationService().getLocation(398);
+    final Location location = Context.getLocationService().getLocation(398);
 
-		System.out.println(location.getName());
-		final Date startDate = DateUtil.getDateTime(2020, 6, 21);
-		final Date endDate = DateUtil.getDateTime(2020, 12, 20);
+    System.out.println(location.getName());
+    final Date startDate = DateUtil.getDateTime(2020, 6, 21);
+    final Date endDate = DateUtil.getDateTime(2020, 12, 20);
 
-		System.out.println(startDate);
-		System.out.println(endDate);
+    System.out.println(startDate);
+    System.out.println(endDate);
 
-		final Map<Parameter, Object> parameters = new HashMap<>();
-		parameters.put(new Parameter("startDate", "Start Date", Date.class), startDate);
-		parameters.put(new Parameter("endDate", "End Date", Date.class), endDate);
-		parameters.put(new Parameter("location", "Location", Location.class), location);
+    final Map<Parameter, Object> parameters = new HashMap<>();
+    parameters.put(new Parameter("startDate", "Start Date", Date.class), startDate);
+    parameters.put(new Parameter("endDate", "End Date", Date.class), endDate);
+    parameters.put(new Parameter("location", "Location", Location.class), location);
 
-		CohortDefinition patientsWhoExperiencedIIT = txRTTCohortQueries.getPatientsOnRTT();
-		CohortDefinition cohortDefinitionC1 = this.resumoMensalAPSSCohortQueries
-				.findPatientsWhoAreCurrentlyEnrolledOnArtWithPrevencaoPosetivaD1();
+    CohortDefinition patientsWhoExperiencedIIT = txRTTCohortQueries.getPatientsOnRTT();
+    CohortDefinition cohortDefinitionC1 =
+        this.resumoMensalAPSSCohortQueries
+            .findPatientsWhoAreCurrentlyEnrolledOnArtWithPrevencaoPosetivaD1();
 
-		CohortDefinition numeratorPreviosPeriod = this.txTBCohortQueries.getSpecimenSentCohortDefinition();
+    CohortDefinition numeratorPreviosPeriod =
+        this.txTBCohortQueries.getSpecimenSentCohortDefinition();
 
-		CohortDefinition txCurrWithTPTCompletation = this.tptCompletationCohortQueries.findTxCurrWithTPTCompletation();
+    CohortDefinition txCurrWithTPTCompletation =
+        this.tptCompletationCohortQueries.findTxCurrWithTPTCompletation();
 
-		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(txCurrWithTPTCompletation,
-				parameters);
+    final EvaluatedCohort evaluateCohortDefinition =
+        this.evaluateCohortDefinition(txCurrWithTPTCompletation, parameters);
 
-		System.out.println(evaluateCohortDefinition.getMemberIds().size());
-		assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
+    System.out.println(evaluateCohortDefinition.getMemberIds().size());
+    assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
 
-		System.out.println("----------------------------------");
+    System.out.println("----------------------------------");
 
-		for (int t : evaluateCohortDefinition.getMemberIds()) {
-			System.out.println(t);
-		}
-	}
+    for (int t : evaluateCohortDefinition.getMemberIds()) {
+      System.out.println(t);
+    }
+  }
 
-	// @Override
-	// protected String username() {
-	// return "admin";
-	// }
-	//
-	// @Override
-	// protected String password() {
-	// return "H!$fGH0Mr$";
-	// }
+  // @Override
+  // protected String username() {
+  // return "admin";
+  // }
+  //
+  // @Override
+  // protected String password() {
+  // return "H!$fGH0Mr$";
+  // }
 
-	@Override
-	protected String username() {
-		return "domingos.bernardo";
-	}
+  @Override
+  protected String username() {
+    return "domingos.bernardo";
+  }
 
-	@Override
-	protected String password() {
-		return "dBernardo1";
-	}
+  @Override
+  protected String password() {
+    return "dBernardo1";
+  }
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Date sstartDate = DateUtil.getDateTime(2018, 11, 14);
-		Date lastDate = DateUtil.adjustDate(sstartDate, 7, DurationUnit.MONTHS);
+    Date sstartDate = DateUtil.getDateTime(2018, 11, 14);
+    Date lastDate = DateUtil.adjustDate(sstartDate, 7, DurationUnit.MONTHS);
 
-		System.out.println(lastDate);
-	}
+    System.out.println(lastDate);
+  }
 }
