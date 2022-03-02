0f05ba07b645fdb1a4b5349d88faf12f5e0b9ccb
elizmuchave <eliazardomuchave@gmail.com>
2022-03-02 14:34:39 +0200

    Configure maven surefire to bypass calculation-api-test

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/dimensions/EptsCommonDimensionTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/dimensions/EptsCommonDimensionTest.java
@@ -2,11 +2,7 @@ package org.openmrs.module.eptsreports.reporting.intergrated.library.dimensions;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 import org.junit.Test;
 import org.openmrs.Cohort;
 import org.openmrs.module.eptsreports.reporting.intergrated.utils.DefinitionsTest;
@@ -29,7 +25,6 @@ public class EptsCommonDimensionTest extends DefinitionsTest {
   public void genderShouldReturnRightCountsPerGender() throws EvaluationException {
     Map<String, Cohort> genderOptions =
         evaluateCohortDefinitionDimension(eptsCommonDimension.gender(), null).getOptionCohorts();
-
     assertEquals(new HashSet<>(Arrays.asList(2, 6)), genderOptions.get("M").getMemberIds());
     assertEquals(new HashSet<>(Arrays.asList(7, 8)), genderOptions.get("F").getMemberIds());
     assertEquals(new HashSet<>(Arrays.asList()), genderOptions.get("?").getMemberIds());
