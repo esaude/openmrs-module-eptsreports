438b7fb0b0a7baf59050511bf323ba51f0ebab5b
eliazmuchave <85493556+eliazmuchave@users.noreply.github.com>
2022-04-26 09:34:37 +0200

    Mepts | 1004 Migrate Livetests Work Under 2x (#744)
    * Fix failing dependency
    
    * Configure maven surefire to bypass calculation-api-test
    
    * Fix EptsCommonDimensionTest#genderShouldReturnRightCountsPerGender
    
    * Fix EptsCommonDimensionTests#ageShouldReturnRightAgeCountsForStandardPatients
    
    * Remove pricise field from metadata.xml
    
    * Add obs status on genericCohortQueriesTest.xml data
    
    * Add obs status on usMonthlySummaryHivCohortQueriesTest.xml
    
    * Fix UsMonthlySummaryHivCohorQueries tests
    
    * Fix QualityImprovementCohortQueiriesTest
    
    * Fix QualityImprovement2020CohortQueriesTest test
    
    * Fix APSSResumoTrimestralTest test
    
    * Fix TxCurrCohortQueriesTest
    
    * Fix ResumoMensal Dataset - Obs table struture
    
    * Fix TxNewCohortQueriesTest - add status on obs table
    
    * Fix CompletedIsoniazidProphylaticTreatmentCalculationTest
    
    * Fix ConsultationUntilEndDateAfterStartingARTTest
    
    * Fix MohMQInitiatedARTDuringTheInclusionPeriodCalculationTest
    
    * Fix ThirdFollowingEncounterAfterOldestARTStartDateCalculationTest
    
    * Fix CXCASRNTest
    
    * Fixing CXCASCRNCaldulationBBTests
    
    * Fixing EPTSCalculationServiceTest
    
    * Fixing GenericCohortTest
    
    * Fixing GenericCohortQueriesTest
    
    * Fixing GenericCohortTests
    
    * remover jdbc dependency added for test propose
    
    * Changig CalculationChortDefinitionEvaluetor to get all patients from patientService
    
    * Skipping LiveTests on mvn plugin tests execution
    
    * Skipping CompletedIsoniazidProphylaticTreatmentCalculationTest
    
    * Enabling testing on CI
    
    * Changing authentication mode and behavior after testing

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/utils/DefinitionsFGHLiveTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/utils/DefinitionsFGHLiveTest.java
@@ -2,8 +2,9 @@ package org.openmrs.module.eptsreports.reporting.intergrated.utils;
 
 import org.junit.Before;
 import org.openmrs.Location;
-import org.openmrs.api.context.Context;
 import org.openmrs.api.context.ContextAuthenticationException;
+import org.openmrs.api.context.Credentials;
+import org.openmrs.api.context.UsernamePasswordCredentials;
 import org.openmrs.module.reporting.common.DateUtil;
 import org.openmrs.test.BaseContextSensitiveTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,17 +14,17 @@ public abstract class DefinitionsFGHLiveTest extends DefinitionsTest {
   /** @see BaseContextSensitiveTest#useInMemoryDatabase() */
   @Override
   public Boolean useInMemoryDatabase() {
-    /*
-     * ensure ~/.OpenMRS/openmrs-runtime.properties exists with your properties
-     * such as; connection.username=openmrs
-     * connection.url=jdbc:mysql://127.0.0.1:3316/openmrs
-     * connection.password=wTV.Tpp0|Q&c
-     */
     return false;
   }
 
   @Override
   public void executeDataSet(String datasetFilename) {}
+
+  @Override
+  public void deleteAllData(){};
+
+  @Override
+  protected Credentials getCredentials() {return new UsernamePasswordCredentials(username(),password());}
 
   protected abstract String username();
 
@@ -31,7 +32,7 @@ public abstract class DefinitionsFGHLiveTest extends DefinitionsTest {
 
   @Before
   public void initialize() throws ContextAuthenticationException {
-    Context.authenticate(username(), password());
+    authenticate();
     setStartDate(DateUtil.getDateTime(2013, 2, 6));
     setEndDate(DateUtil.getDateTime(2019, 3, 6));
     setLocation(new Location(103));
