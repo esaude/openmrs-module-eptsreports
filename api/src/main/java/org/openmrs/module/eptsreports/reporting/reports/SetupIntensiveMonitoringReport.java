5bf1b265dfda8f4464d7289d403a5cf18ad9c0ca
Constantino De Jesus Muianga <57292083+TinoMuianga@users.noreply.github.com>
2023-04-20 09:49:05 +0200

    Removing duplicated indicator for Cat 11. on dataset (#927)

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupIntensiveMonitoringReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupIntensiveMonitoringReport.java
@@ -51,7 +51,7 @@ public class SetupIntensiveMonitoringReport extends EptsDataExportManager {
 
   @Override
   public String getName() {
-    return "Monitoria Intensiva de HIV - 2022";
+    return "Monitoria Intensiva de HIV - 2023";
   }
 
   @Override
